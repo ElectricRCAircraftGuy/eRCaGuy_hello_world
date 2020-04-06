b08ffc826c526daab65c41b2f5bb479a1e67cf7f
Gabriel Staples <ercaguy@gmail.com>
2020-04-05 19:49:38 -0700

    Add .gitignore file to ignore bin/ folders; update run_tests.sh script to be able to be run from anywhere

--- c/rounding_integer_division/run_tests.sh
+++ c/rounding_integer_division/run_tests.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+THIS_PATH="$(realpath $0)"
+# echo "THIS_PATH = \"$THIS_PATH\"" # for debugging
+THIS_DIR="$(dirname "$THIS_PATH")"
+# echo "THIS_DIR = \"$THIS_DIR\"" # for debugging 
+
 echo "=========================="
 echo "1/2: C tests"
-echo "gcc -Wall -Werror -g3 -std=c11 -o rounding_integer_division rounding_integer_division.c && ./rounding_integer_division"
 echo "=========================="
 echo ""
-gcc -Wall -Werror -g3 -std=c11 -o rounding_integer_division rounding_integer_division.c && ./rounding_integer_division
+gcc -Wall -Werror -g3 -std=c11 -o "$THIS_DIR/bin/rounding_integer_division_c" "$THIS_DIR/rounding_integer_division.c" && "$THIS_DIR/bin/rounding_integer_division_c"
 
 echo ""
 echo ""
 echo "=========================="
 echo "2/2: C++ tests"
-echo "g++ -Wall -Werror -g3 -std=c++17 -o rounding_integer_division rounding_integer_division.cpp && ./rounding_integer_division"
 echo "=========================="
 echo ""
-g++ -Wall -Werror -g3 -std=c++17 -o rounding_integer_division rounding_integer_division.cpp && ./rounding_integer_division
+g++ -Wall -Werror -g3 -std=c++17 -o "$THIS_DIR/bin/rounding_integer_division_cpp" "$THIS_DIR/rounding_integer_division.cpp" && "$THIS_DIR/bin/rounding_integer_division_cpp"
 
