3e6f3e38e41a1562ab42d614680a9f43a39e915d
Gabriel Staples <ercaguy@gmail.com>
2020-04-26 15:35:16 -0700

    wip: add various templates in cpp/template_practice/explicit_template_specialization.cpp
    So far so good! It all works as expected! I have the following function
    templates: `divide1` + 1 template specialization, `divide2`, `divide3`

--- cpp/template_practice/explicit_template_specialization.cpp
+++ cpp/template_practice/explicit_template_specialization.cpp
@@ -28,11 +28,143 @@ See the following files for additional general info. on building and running:
 // C++ includes
 #include <iostream>
 
+// 1. A normal function template based on type, with numer & denom passed in as 
+// **function parameter inputs** **of a given template type, T**
+template <typename T>
+T divide1(T numer, T denom)
+{
+    // Does NOT work, because `denom` cannot be evaluated at compile time since it is an input
+    // parameter **to the function** NOT a static/compile-time **template parameter**!
+    //      explicit_template_specialization.cpp:38:5: error: non-constant condition for static assertion
+    //      explicit_template_specialization.cpp:38:5: error: ‘denom’ is not a constant expression
+    // static_assert(denom != 0, "dividing by zero not allowed");
+
+    // WORKS--run-time check!
+    if (denom == 0)
+    {
+        printf("divide1: cannot divide by zero\n");
+        return 0;
+    }
+
+    return numer/denom;
+}
+
+// Template specialization; see: https://en.wikipedia.org/wiki/Template_(C%2B%2B)#Explicit_template_specialization
+template <>
+double divide1(double numer, double denom)
+{
+    printf("divide1 double type: just returning -0.1419\n");
+    return -0.1419;
+}
+
+
+// 2. A function template based on static/compile-time **template input**, with numer & denom
+// passed in as **template parameter inputs**
+template <int numer, int denom>
+int divide2()
+{
+    // WORKS--compile-time check!
+    // This works because numer and denom are both **template parameters**, so they get inserted
+    // into their own versions of this function **at compile time**!
+    // static_assert(denom != 0, "dividing by zero not allowed");
+    
+    // WORKS--run-time check!
+    if (denom == 0)
+    {
+        printf("divide2: cannot divide by zero\n");
+        return 0;
+    }
+
+    return numer/denom;
+}
+
+// 3. A function template based on static/compile-time **template inputs**, with numer & denom
+// passed in as **template parameter inputs**, and having their types specified by the **first
+// template parameter type, T**
+template <typename T, T numer, T denom>
+T divide3()
+{
+    // WORKS--compile-time check!
+    // This works because numer and denom are both **template parameters**, so they get inserted
+    // into their own versions of this function **at compile time**!
+    // static_assert(denom != 0, "dividing by zero not allowed");
+    
+    // WORKS--run-time check!
+    if (denom == 0)
+    {
+        printf("divide3: cannot divide by zero\n");
+        return 0;
+    }
+
+    return numer/denom;
+}
+
+// // 3. A function template based on static/compile-time **template inputs**, with numer & denom
+// // passed in as **template parameter inputs**, and having their types specified by the **first
+// // template parameter type, T**
+// template <typename T, T numer, T denom>
+// T divide3()
+// {
+//     // WORKS--compile-time check!
+//     // This works because numer and denom are both **template parameters**, so they get inserted
+//     // into their own versions of this function **at compile time**!
+//     // static_assert(denom != 0, "dividing by zero not allowed");
+    
+//     // WORKS--run-time check!
+//     if (denom == 0)
+//     {
+//         printf("divide3: cannot divide by zero\n");
+//         return 0;
+//     }
+
+//     return numer/denom;
+// }
+
+
 
 
 int main()
 {
     printf("Hello World\n");
+
+    int i1;
+    int i2;
+
+    // 1. 
+    printf("\ndivide1\n============\n");
+
+    i1 = divide1<int>(25, 2);
+    printf("i1 = %i\n", i1); // 12
+
+    i2 = divide1<int>(25, 0);
+    printf("i2 = %i\n", i2); 
+
+    printf("divide1(25.0, 0.0) = %f\n", divide1(25.0, 0.0));
+
+    // for (int i = 10; i >= 0; i--)
+    // {
+    //     int ans = divide(100, i);
+    //     printf("  ans = %i\n", ans);
+    // }
+
+    // 2. 
+    printf("\ndivide2\n============\n");
+
+    i1 = divide2<25, 2>();
+    printf("i1 = %i\n", i1); // 12
+
+    i2 = divide2<25, 0>();
+    printf("i2 = %i\n", i2); 
+
+    // 3. 
+    printf("\ndivide3\n============\n");
+
+    i1 = divide3<int, 31, 2>();
+    printf("i1 = %i\n", i1); // 12
+
+    i2 = divide3<int, 31, 0>();
+    printf("i2 = %i\n", i2); 
+
 
     return 0;
 }
