2274333d08fc8e71132d37c72e4e2c4afc9f263f
Gabriel Staples <ercaguy@gmail.com>
2020-04-29 21:04:23 -0700

    Add cpp/struct_initialization.cpp
    Note that this program is still a work in progress (wip), as it doesn't
    quite work right yet, esp. for zero-initialization!

---
+++ cpp/run_struct_initialization.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+# Obtain path to this script.
+# See my own ans here: 
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself/60157372#60157372
+THIS_PATH="$(realpath $0)"
+THIS_DIR="$(dirname "$THIS_PATH")"
+# echo "THIS_DIR = \"$THIS_DIR\"" # for debugging 
+
+# Build and run this program
+
+PROGRAM_NAME="struct_initialization"
+SOURCE_FILES="struct_initialization.cpp"
+SOURCE_FILES_C="struct_initialization.c"
+BIN_DIR="bin"
+# BIN_DIR="bin_hello_world"
+
+mkdir -p "${THIS_DIR}/${BIN_DIR}"
+
+# - See all warning options in the gcc user manual here on the "Warning Options" page: 
+#   https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
+# - Note: `-g3` adds full debugging info! See the `-g` and `-glevel` options here: 
+#   https://gcc.gnu.org/onlinedocs/gcc/Debugging-Options.html
+# - For meaning of -save-temps=obj, see: https://stackoverflow.com/a/46256372/4561887
+#   Basically, it tells the temp objects to go to the same location as the -o setting!
+
+echo "======================"
+echo "Running first as C++17"
+echo "======================"
+
+g++ -Wall -Werror -g3 -std=c++17 -save-temps=obj -o "${THIS_DIR}/${BIN_DIR}/${PROGRAM_NAME}" \
+"${THIS_DIR}/${SOURCE_FILES}" && \
+"${THIS_DIR}/${BIN_DIR}/${PROGRAM_NAME}"
+
+echo "======================"
+echo "Now running as C11"
+echo "======================"
+
+gcc -Wall -Werror -g3 -std=c11 -save-temps=obj -o "${THIS_DIR}/${BIN_DIR}/${PROGRAM_NAME}" \
+"${THIS_DIR}/${SOURCE_FILES_C}" && \
+"${THIS_DIR}/${BIN_DIR}/${PROGRAM_NAME}"
---
+++ cpp/struct_initialization.cpp
@@ -0,0 +1,185 @@
+/*
+
+This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+Gabriel Staples
+Written: 8 Nov. 2019 
+Updated: 29 Apr. 2020 
+
+struct_initialization
+- practice aggregate initialization and zero initialization of structs in C++
+
+******NB: THIS IS A WORK IN PROGRESS! MOST OF THESE ZERO INITIALIZATION TESTS FAIL!******
+
+To compile and run:
+    g++ -Wall -Werror -g3 -std=c++17 -o struct_initialization \
+    struct_initialization.cpp && ./struct_initialization
+
+References:
+1. https://en.cppreference.com/w/cpp/language/aggregate_initialization
+1. https://en.cppreference.com/w/cpp/language/zero_initialization
+
+*/
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+typedef struct
+{
+    int num1 = 100;
+    int num2 = -100;
+    int num3;
+} data_t;
+#else // For C only, since C doesn't allow default values in structs 
+typedef struct
+{
+    int num1;
+    int num2;
+    int num3;
+} data_t;
+#endif 
+
+int main()
+{
+    printf("Hello World\n\n");
+    
+    // 1. Aggregate Initialization
+    // See: https://en.cppreference.com/w/cpp/language/aggregate_initialization
+    printf("TEST 1: aggregate initialization\n\n");
+    
+    // THIS WORKS IN C! 
+    // IT ALSO WORKS IN C++! NOTE, HOWEVER, THAT IN C++ IT *MAY* BE A GCC 
+    // EXTENSION, MEANING IT'S NOT PORTABLE C++ CODE. DO SOME CHECKING AND 
+    // RESEARCH ONLINE TO FIND OUT! 
+    // Update 29 Apr. 2020: yes, it must be a gcc extension, as I'm building 
+    // with C++17, yet this reference (https://en.cppreference.com/w/cpp/language/aggregate_initialization)
+    // indicates this initialization style is not possible until C++20.
+    data_t d1 = 
+    {
+        .num1 = 7,
+        .num2 = 8,
+    };
+
+    printf("d1.num1 = %i\nd1.num2 = %i\nd1.num3 = %i\n\n", d1.num1, d1.num2, d1.num3);
+
+
+    // 2. Zero Initialization
+    // See: https://en.cppreference.com/w/cpp/language/zero_initialization
+    // ******NB: THIS IS A WORK IN PROGRESS! MOST OF THESE ZERO INITIALIZATION TESTS FAIL!******
+    printf("TEST 2: zero initialization\n\n");
+    
+    data_t d2 = {};
+    printf("d2.num1 = %i\nd2.num2 = %i\nd2.num3 = %i\n\n", d2.num1, d2.num2, d2.num3);
+    
+    data_t d3 = {0};
+    printf("d3.num1 = %i\nd3.num2 = %i\nd3.num3 = %i\n\n", d3.num1, d3.num2, d3.num3);
+    
+    // 3. More Initialization tests
+    // See: https://en.cppreference.com/w/cpp/language/zero_initialization
+    printf("TEST 3: more initialization tests\n\n");
+
+    data_t d4 = {2};
+    printf("d4.num1 = %i\nd4.num2 = %i\nd4.num3 = %i\n\n", d4.num1, d4.num2, d4.num3);
+    
+    #ifdef __cplusplus
+    printf("The following techniques only are valid in C++, NOT C, so disable them for C:\n{\n\n");
+
+    data_t d5;
+    printf("d5.num1 = %i\nd5.num2 = %i\nd5.num3 = %i\n\n", d5.num1, d5.num2, d5.num3);
+
+    data_t d6{};
+    printf("d6.num1 = %i\nd6.num2 = %i\nd6.num3 = %i\n\n", d6.num1, d6.num2, d6.num3);
+
+    data_t d7 = data_t();
+    printf("d7.num1 = %i\nd7.num2 = %i\nd7.num3 = %i\n\n", d7.num1, d7.num2, d7.num3);
+
+    // // Error: 
+    // //      error: no matching function for call to ‘data_t::data_t(int)’
+    // data_t d8 = data_t(15);
+    // printf("d8.num1 = %i\nd8.num2 = %i\nd8.num3 = %i\n\n", d8.num1, d8.num2, d8.num3);
+
+    printf("}\n\n");
+    #endif
+
+    return 0;
+}
+
+/*
+SAMPLE OUTPUT:
+
+    $ ./run_struct_initialization.sh 
+    ======================
+    Running first as C++17
+    ======================
+    Hello World
+
+    TEST 1: aggregate initialization
+
+    d1.num1 = 7
+    d1.num2 = 8
+    d1.num3 = 0
+
+    TEST 2: zero initialization
+
+    d2.num1 = 100
+    d2.num2 = -100
+    d2.num3 = 0
+
+    d3.num1 = 0
+    d3.num2 = -100
+    d3.num3 = 0
+
+    TEST 3: more initialization tests
+
+    d4.num1 = 2
+    d4.num2 = -100
+    d4.num3 = 0
+
+    The following techniques only are valid in C++, NOT C, so disable them for C:
+    {
+
+    d5.num1 = 100
+    d5.num2 = -100
+    d5.num3 = 0
+
+    d6.num1 = 100
+    d6.num2 = -100
+    d6.num3 = 0
+
+    d7.num1 = 100
+    d7.num2 = -100
+    d7.num3 = 0
+
+    }
+
+    ======================
+    Now running as C11
+    ======================
+    Hello World
+
+    TEST 1: aggregate initialization
+
+    d1.num1 = 7
+    d1.num2 = 8
+    d1.num3 = 0
+
+    TEST 2: zero initialization
+
+    d2.num1 = 0
+    d2.num2 = 0
+    d2.num3 = 0
+
+    d3.num1 = 0
+    d3.num2 = 0
+    d3.num3 = 0
+
+    TEST 3: more initialization tests
+
+    d4.num1 = 2
+    d4.num2 = 0
+    d4.num3 = 0
+
+
+
+*/
+
