c8cbbe1a26d336b95599060f2b04c9b2e53dbaa7
Gabriel Staples <ercaguy@gmail.com>
2020-10-22 00:08:02 -0700

    onlinegdb--strncmpci.c: wip: begin adding unit test functions

--- c/onlinegdb--strncmpci.c
+++ c/onlinegdb--strncmpci.c
@@ -21,31 +21,26 @@ References:
 3. http://www.cplusplus.com/reference/cstring/strncmp/
 
 STATUS:
-Done. It works!
+WORK-IN-PROGRESS!
 
 */
 
+#include <assert.h>
+#include <stdbool.h>
 #include <ctype.h> // for `tolower()`
 #include <limits.h> // for `INT_MIN`
 #include <stdio.h>
 #include <string.h>
 
-/*
+typedef struct data_s
+{
+    int error_count;
+} data_t;
 
-Case-insensitive string compare (strncmp case-insensitive)
-- Identical to strncmp except case-insensitive. See: http://www.cplusplus.com/reference/cstring/strncmp/
-- Aided/inspired, in part, by: https://stackoverflow.com/a/5820991/4561887
-
-
-
-
-return:
-(essentially identical to strncmp)
-INT_MIN  invalid arguments (one or both of the input strings is a NULL pointer)
-<0       the first character that does not match has a lower value in str1 than in str2
- 0       the contents of both strings are equal
->0       the first character that does not match has a greater value in str1 than in str2
-*/
+// Data struct used to safely contain and pass around global data
+data_t globals = {
+    .error_count = 0,
+};
 
 /// \brief      Perform a case-insensitive string compare (`strncmp()` case-insensitive) to see
 ///             if two C-strings are equal.
@@ -102,21 +97,88 @@ done:
     return ret_code;
 }
 
-// Added 26 July 2020
-// Copied directly from:
-// https://stackoverflow.com/questions/5820810/case-insensitive-string-comp-in-c/5820991#5820991
-int strcicmp(char const *a, char const *b)
+// TODO: ADD IN Unit tests to test this function too! Ex: `EXPECT_EQUALS(strcicmp(str1, str2), 0);`
+
+// /// \brief      Alternative approach to test and compare results from.
+// /// \note       Copied directly from here:
+// ///             https://stackoverflow.com/questions/5820810/case-insensitive-string-comp-in-c/5820991#5820991
+// int strcicmp(char const *a, char const *b)
+// {
+//     for (;; a++, b++) {
+//         int d = tolower((unsigned char)*a) - tolower((unsigned char)*b);
+//         if (d != 0 || !*a)
+//             return d;
+//     }
+// }
+
+/// \brief      Wrapper around the below unit test function.
+/// \details    Sample usage:
+///                 EXPECT_EQUALS(strncmpci(str1, str2, n), 1);
+///             Sample output:
+///                 FAILED at line 173 in function main! strncmpci(str1, str2, n) != 1
+///                   strncmpci(str1, str2, n) is 0
+///                   1 is 1
+#define EXPECT_EQUALS(int_a, int_b) \
+    do { \
+        expect_equals(int_a, int_b, &globals.error_count, #int_a, #int_b, __LINE__, __func__); \
+    } while (false)
+
+/// \brief      Perform a simple unit test to see if int a == int b.
+/// \param[in]      a            the first integer to compare
+/// \param[in]      b            the second integer to compare
+/// \param[in,out]  error_count  (Optional) a total error counter which will be incremented in the
+///                     event a != b. Pass in NULL to not use.
+/// \param[in]      a_str        (Optional) a string to print to represent what was passed in for
+///                              `a`. Pass in NULL to not use.
+/// \param[in]      b_str        (Optional) a string to print to represent what was passed in for
+///                              `b`. Pass in NULL to not use.
+/// \param[in]      line         The line number of the call site; pass in `__LINE__`. See:
+///                              https://gcc.gnu.org/onlinedocs/cpp/Standard-Predefined-Macros.html.
+/// \param[in]      func         The function of the call site; pass in `__func__`. See:
+///                              https://gcc.gnu.org/onlinedocs/gcc/Function-Names.html.
+/// \return     true if a == b, and false otherwise
+bool expect_equals(int a, int b, int * error_count, char * a_str, char * b_str, int line,
+    const char * func)
 {
-    for (;; a++, b++) {
-        int d = tolower((unsigned char)*a) - tolower((unsigned char)*b);
-        if (d != 0 || !*a)
-            return d;
+    if (a == b)
+    {
+        return true;
     }
+
+    if (error_count != NULL)
+    {
+        (*error_count)++;
+    }
+
+    if (a_str == NULL || b_str == NULL)
+    {
+        printf("FAILED at line %i in function %s! a != b\n"
+               "  a is %i\n"
+               "  b is %i\n\n",
+               line, func, a, b);
+    }
+    else
+    {
+        // both a_str and b_str are NOT null ptrs
+        printf("FAILED at line %i in function %s! %s != %s\n"
+               "  %s is %i\n"
+               "  %s is %i\n\n",
+               line, func, a_str, b_str, a_str, a, b_str, b);
+    }
+
+    return false;
 }
 
 int main()
 {
-    printf("Hello World\n\n");
+    printf("String Comparison Tests\n\n");
+
+    int num_failures_expected = 0;
+
+    printf("Intentional unit test failure to show what a unit test failure looks like!\n");
+    EXPECT_EQUALS(strncmpci("hey", "HEY", 3), 1);
+    num_failures_expected++;
+
 
     const char * str1;
     const char * str2;
@@ -125,16 +187,14 @@ int main()
     str1 = "hey";
     str2 = "HEY";
     n = 3;
-    printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
-    printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
-    printf("\n");
+    EXPECT_EQUALS(strncmpci(str1, str2, 3), 0);
+    EXPECT_EQUALS(strncmp(str1, str2, 3), 32);
 
     str1 = "heY";
     str2 = "HeY";
     n = 3;
     printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
+    // printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
     printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
     printf("\n");
 
@@ -142,7 +202,7 @@ int main()
     str2 = "HEdY";
     n = 3;
     printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
+    // printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
     printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
     printf("\n");
 
@@ -150,7 +210,7 @@ int main()
     str2 = "HeYd";
     n = 3;
     printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
+    // printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
     printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
     printf("\n");
 
@@ -158,7 +218,7 @@ int main()
     str2 = "HeYd";
     n = 6;
     printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
+    // printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
     printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
     printf("\n");
 
@@ -166,7 +226,7 @@ int main()
     str2 = "hey";
     n = 6;
     printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
+    // printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
     printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
     printf("\n");
 
@@ -174,7 +234,7 @@ int main()
     str2 = "heyd";
     n = 6;
     printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
+    // printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
     printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
     printf("\n");
 
@@ -182,11 +242,14 @@ int main()
     str2 = "heyd";
     n = 3;
     printf("strncmpci(%s, %s, %lu) = %i\n", str1, str2, n, strncmpci(str1, str2, n));
-    printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
+    // printf("strcicmp(%s, %s) = %i\n", str1, str2, strcicmp(str1, str2));
     printf("strncmp(%s, %s, %lu) = %i\n", str1, str2, n, strncmp(str1, str2, n));
     printf("\n");
 
-    return 0;
+
+    assert(globals.error_count == num_failures_expected);
+
+    return globals.error_count;
 }
 
 /*
