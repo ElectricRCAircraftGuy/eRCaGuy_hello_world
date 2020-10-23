0ffa04485523c0c3ba09d5adf3d1d25c7cd38649
Gabriel Staples <ercaguy@gmail.com>
2020-10-22 22:07:12 -0700

    strncmpci.c: remove `goto`
    Remove `goto` from the main function, but copy the function and retain
    it in a second version of the function.
    
    This example code has super simple error handling, and goto isn't
    required. goto is a very valid, good, and valuable tool to make clean,
    safe and readable error-handling code in C, _especially_ for the really
    complicated cases where you are initializing a ton of hardware and need
    to to different amounts of cleanup, and in different ways, if something
    fails during this long initialization process.
    
    Nevertheless, some people may be turned off by it because they don't
    understand it and they've been misinformed about it their whole lives
    and told it's evil. For this super simple case, there's no sense in
    having to try to convince people, so just change it.
    
    Note to anyone who's curious: here's some of the valid justification
    I've written about for the virtues of goto. See my little blurb at the
    bottom of my answer here, titled
    
    ------------------------------------------------------------------------
    **"Additional reading and justification
    for valid usage of `goto` in error handling for professional code":**
    
    https://stackoverflow.com/a/54488289/4561887.
    ------------------------------------------------------------------------

--- c/strncmpci.c
+++ c/strncmpci.c
@@ -84,6 +84,65 @@ int strncmpci(const char * str1, const char * str2, size_t num)
     if (!str1 || !str2)
     {
         ret_code = INT_MIN;
+        return ret_code;
+    }
+
+    // Continue doing case-insensitive comparisons, one-character-at-a-time, of `str1` to `str2`,
+    // as long as at least one of the strings still has more characters in it, and we have
+    // not yet compared `num` chars.
+    while ((*str1 || *str2) && (chars_compared < num))
+    {
+        ret_code = tolower((int)(*str1)) - tolower((int)(*str2));
+        if (ret_code != 0)
+        {
+            // The 2 chars just compared don't match
+            break;
+        }
+        chars_compared++;
+        str1++;
+        str2++;
+    }
+
+    return ret_code;
+}
+
+// VERSION **WITH** GOTO. This is perfectly valid and safe usage of `goto`, but some people
+// may have a problem with it, and it's suuuuper easy to avoid in this simple example code,
+// so let's remove it for the main version above but leave it for this version below.
+
+/// \brief      Perform a case-insensitive string compare (`strncmp()` case-insensitive) to see
+///             if two C-strings are equal.
+/// \note       1. Identical to `strncmp()` except:
+///               1. It is case-insensitive.
+///               2. The behavior is NOT undefined (it is well-defined) if either string is a null
+///               ptr. Regular `strncmp()` has undefined behavior if either string is a null ptr
+///               (see: https://en.cppreference.com/w/cpp/string/byte/strncmp).
+///               3. It returns `INT_MIN` as a special sentinel value for certain errors.
+///             - Posted as an answer here: https://stackoverflow.com/a/55293507/4561887.
+///               - Aided/inspired, in part, by `strcicmp()` here:
+///                 https://stackoverflow.com/a/5820991/4561887.
+/// \param[in]  str1        C string 1 to be compared.
+/// \param[in]  str2        C string 2 to be compared.
+/// \param[in]  num         max number of chars to compare
+/// \return     A comparison code (identical to `strncmp()`, except with the addition
+///             of `INT_MIN` as a special sentinel value):
+///
+///             INT_MIN (usually -2147483648 for int32_t integers)  Invalid arguments (one or both
+///                      of the input strings is a NULL pointer).
+///             <0       The first character that does not match has a lower value in str1 than
+///                      in str2.
+///              0       The contents of both strings are equal.
+///             >0       The first character that does not match has a greater value in str1 than
+///                      in str2.
+int strncmpci2(const char * str1, const char * str2, size_t num)
+{
+    int ret_code = 0;
+    size_t chars_compared = 0;
+
+    // Check for NULL pointers
+    if (!str1 || !str2)
+    {
+        ret_code = INT_MIN;
         goto done;
     }
 
