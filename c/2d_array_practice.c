6de6263d3cf4101cff072b0a3a0f77373f22addb
Gabriel Staples <ercaguy@gmail.com>
2021-06-03 16:59:50 -0700

    2d_array_practice.c: add ptr to array type-safety tests, & doxygen

--- c/2d_array_practice.c
+++ c/2d_array_practice.c
@@ -8,7 +8,9 @@ www.ElectricRCAircraftGuy.com
 
 Learn and practice a few ways to use and pass multi-dimensional (ex: 2D) arrays in C.
 See my final answers here:
-1. [my main answer] https://stackoverflow.com/a/67814330/4561887
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+1. [my main answer] https://stackoverflow.com/a/67814330/4561887                 <======== READ & STUDY THIS FOR FULL UNDERSTANDING OF THIS CODE
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 2. [my answer which is a snippet of the one above]
    https://stackoverflow.com/a/67814569/4561887
 
@@ -108,62 +110,38 @@ int* all_rows[] = {row1, row2, row3};
 /// Get number of columns in a 2D array
 #define NUM_COLS(array_2d) ARRAY_LEN(array_2d[0])
 
-/*
-LEARNING NOTES:
 
-1. Fixed vs unspecified dimensions:
+/// A function to demonstrate that the `3` below in the index has ZERO influence
+/// on this function!
+void print_sizeof_1d_array(int arr[3])
+{
+    // Not allowed!
+    //      error: ‘sizeof’ on array function parameter ‘arr’ will return size
+    //      of ‘int *’ [-Werror=sizeof-array-argument]
+    // printf("sizeof(arr) = %zu\n", sizeof(arr));
 
-Arrays must be **fixed size** on all dimensions except the 1st dimension, which
-can be unspecified.
+    // Just takes the size of an int, which is 4 for my architecture!
+    // Ignores the `3` above, since `arr` actually decayed into type `int *`!
+    printf("sizeof(*arr) = %zu\n", sizeof(*arr));
+}
 
-    // OK; 1D arrray with unspecified 1st (and only) dimension
-    int array[] = {1, 2, 3};
-    // OK; 2D array with unspecified 1st dimensions
-    int array[][2] = {{1, 2}, {3, 4}};
-    // NOT allowed; 2D array with both dimensions unspecified!:
-    // `error: array type has incomplete element type ‘int[]’`
-    int array[][] = {{1, 2}, {3, 4}};
-
-2. Type decay:
-
-When used as function parameters, all non-pointer array types A moment to explain type decay: array types with unspecified-size dimensions
-naturally decay down to a pointer in place of that one unspecified-size
-dimension. So, although `int arr[]` (array of ints) and `int * arr` (pointer to
-an int) are NOT the same types, a function definition with either of those in
-it will naturally decay the unspecified-size array dimension down to a pointer,
-resulting in type (`int * arr`) being passed to the function:
-
-// accepts `int *` as `array` parameter
-void my_func2(int * array, size_t len) {}
-// also accepts `int *` as `array` parameter, since the `int []` type (array of
-// ints) naturally decays down to type `int *` (ptr to int`).
-void my_func1(int array[], size_t len) {}
-
-Taking this further, adding a
-
-void my_func3(int array[1], size_t len) {}   // same as above: accepts `int *` as 1st parameter
-void my_func4(int array[10], size_t len) {}  // same as above: accepts `int *` as 1st parameter
-void my_func5(int array[100], size_t len) {} // same as above: accepts `int *` as 1st parameter
+void my_func(int (*array)[10])
+{
+    // suppress warning that this param is unused (`-Werror=unused-parameter`)
+    (void)array;
+}
 
 
-int array[10];
-my_func1(array); // ok; `array` naturally decays down to type `int *`
-
-*/
-
-
-
-/// \brief      Print a 2D array which has a VARIABLE number of rows and
+/// \brief      Print a 2D array which has a VARIABLE number of rows but
 ///             FIXED number of columns.
 /// \details    Don't use this approach at all, ever. It has a superfluous
 ///             `num_cols` parameter that we don't actually need! (see the
 ///             `print_array3` approach below instead for how we can get rid of
 ///             this). So, this approach is shown here for demonstration
 ///             purposes only.
-/// \param[in]  array_2d    a 2D array; `int array_2d[][2]` is a 2D array with
-///     variable 1st dimension (rows) and size-2 2nd dimension (columns).
-///     NB: since the 1st dimension is NOT a fixed size, this array type
-///     naturally decays to type `int (*)[2]`, or ptr to array of 2 ints.
+/// \param[in]  array_2d    a 2D array; is of type `int [][2]` (n x 2 (2D) array
+///                         of ints), which naturally decays to type
+///                         `int (*)[2]` (ptr to (1D) array of 2 ints)
 /// \param[in]  num_rows    The number of rows in the array
 /// \param[in]  num_cols    The number of columns in the array
 /// \return     None
@@ -181,11 +159,19 @@ void print_array1(const int array_2d[][2], size_t num_rows, size_t num_cols)
     printf("\n");
 }
 
-/// Better way (WITH array size type safety on array size)
-/// `array_2d` here is a ptr to an array of size[3][2]. This **forces** type safety in C based on array
-/// size. See my answer here: https://stackoverflow.com/a/51527502/4561887
-/// I think this is overly-complicated, however (again, read my answer just above), so let's
-/// do another approach later withOUT type safety.
+// Better way (WITH array size type safety on array size)
+// `array_2d` here is a ptr to an array of size[3][2]. This **forces** type safety in C based on array
+// size. See my answer here: https://stackoverflow.com/a/51527502/4561887
+// I think this is overly-complicated, however (again, read my answer just above), so let's
+// do another approach later withOUT type safety.
+//
+/// \brief      Print a 2D array which has a FIXED number of rows and
+///             FIXED number of columns.
+/// \param[in]  array_2d    a 2D array; is of type `int (*)[3][2]` (ptr to
+///                         3 x 2 (2D) array of ints); since it is already
+///                         explicitly a ptr, it does NOT naturally decay to
+///                         any other type of ptr
+/// \return     None
 void print_array2(const int (*array_2d)[3][2])
 {
     printf("print_array2:\n");
@@ -200,8 +186,16 @@ void print_array2(const int (*array_2d)[3][2])
     printf("\n");
 }
 
-/// Better way (withOUT array size type safety on array size) (my preferred approach between this
-/// one and the one above, UNLESS all arrays are the same size, in which case use the one above)
+// Better way (withOUT array size type safety on array size) (my preferred approach between this
+// one and the one above, UNLESS all arrays are the same size, in which case use the one above)
+//
+/// \brief      Print a 2D array which has a VARIABLE number of rows but
+///             FIXED number of columns.
+/// \param[in]  array_2d    a 2D array; is of type `int [][2]` (n x 2 (2D) array
+///                         of ints), which naturally decays to type
+///                         `int (*)[2]` (ptr to (1D) array of 2 ints)
+/// \param[in]  num_rows    The number of rows in the array
+/// \return     None
 void print_array3(const int array_2d[][2], size_t num_rows)
 {
     printf("print_array3:\n");
@@ -216,8 +210,21 @@ void print_array3(const int array_2d[][2], size_t num_rows)
     printf("\n");
 }
 
-/// Much more-versatile approach. (My overall preferred approach since it's the most-versatile).
-/// NB: `array_2d` is a pointer to the start of a contiguous 2D array of `int`s. So, treat it as such.
+// Much more-versatile approach. (My overall preferred approach since it's the most-versatile).
+// NB: `array_2d` is a pointer to the start of a contiguous 2D array of `int`s. So, treat it as such.
+//
+/// \brief      Print a 2D array which has a VARIABLE number of rows and
+///             VARIABLE number of columns.
+/// \param[in]  array_2d    a 2D array; is of type `int *` (ptr to int); even
+///                         though a 1D array of type `int []` (array of ints)
+///                         naturally decays to this type, don't think about it
+///                         that way; rather, think of it as a ptr to the first
+///                         `int` in a contiguous block of memory containing a
+///                         multidimensional array, and we will manually index
+///                         into it as required and according to its dimensions
+/// \param[in]  num_rows    The number of rows in the array
+/// \param[in]  num_cols    The number of columns in the array
+/// \return     None
 void print_array4(const int *array_2d, size_t num_rows, size_t num_cols)
 {
     printf("print_array4:\n");
@@ -236,14 +243,31 @@ void print_array4(const int *array_2d, size_t num_rows, size_t num_cols)
 }
 
 
-/// Now let's force some code into the mold (prototype) style the OP requested for completeness. `array_2d`
-/// here is different from all of the cases above. It is NOT a contiguous 2D array of `int`s;
-/// rather, it is an array of pointers to ints, where each pointer in the array can be thought of as
-/// a sub-array. Therefore, the length of the outer array is the number of rows, and the length of
-/// each sub-array, or inner array, is the number of columns. Each sub-array (a single row of
-/// `int`s) DOES have to be in contiguous memory, and the array of _pointers_ DOES have to be in
-/// contiguous memory, but the actual _storage space_ for each row can be in NON-contiguous memory.
-/// Again, this is VERY different from every other function above.
+// Now let's force some code into the mold (prototype) style the OP requested for completeness.
+//
+/// \brief      Print a 2D-like array, where the array passed in is an array of
+///             ptrs (int *) to other sub-arrays. Each index into the outer
+///             array is the row, then each index into a sub-array in a given
+///             row is the column. This handles a VARIABLE number of rows and
+///             VARIABLE number of columns.
+/// \details    `array_2d` here is different from all of the cases above. It is
+///             NOT a contiguous 2D array of `int`s; rather, it is an array of
+///             pointers to ints, where each pointer in the array can be
+///             thought of as a sub-array. Therefore, the length of the outer
+///             array is the number of rows, and the length of each sub-array,
+///             or inner array, is the number of columns. Each sub-array
+///             (a single row of `int`s) DOES have to be in contiguous memory,
+///             and the array of _pointers_ DOES have to be in contiguous
+///             memory, but the actual _storage space_ for each row can be in
+///             NON-contiguous memory. Again, this is VERY different from every
+///             other function above.
+/// \param[in]  array_2d    a 2D array; is of type `int * []` (array of ptrs to
+///                         int) (where each ptr is a sub-array of ints)
+/// \param[in]  num_rows    The number of rows in the array (number of elements
+///                         in the `array_2d` outer array)
+/// \param[in]  num_cols    The number of columns in the array (number of
+///                         elements in each sub-array)
+/// \return     None
 void print_array5(const int* array_2d[], size_t num_rows, size_t num_cols)
 {
     printf("print_array5:\n");
@@ -267,6 +291,124 @@ int main()
 {
     printf("hello\n\n");
 
+    // ===========================
+    printf("1D array tests:\n\n");
+    // ===========================
+
+    int array_1d[] = {1, 2, 3};
+    int * array_1d_p = array_1d;
+    print_sizeof_1d_array(array_1d);
+    print_sizeof_1d_array(array_1d_p);
+    printf("\n");
+
+    // 1. Create arrays
+
+    int array1[5];
+    int *array1_p = array1; // array1_p is of type `int *` (ptr to int)
+    int (*array1_p2)[5] = &array1; // array1_p2 is of type `int (*)[5]` (ptr
+                                   // to array of 5 ints)
+    // Suppress `-Werror=unused-variable` errors
+    (void)array1;
+    (void)array1_p;
+    (void)array1_p2;
+
+    int array2[10];
+    int *array2_p = array2; // array2_p is of type `int *` (ptr to int)
+    int (*array2_p2)[10] = &array2; // array2_p2 is of type `int (*)[10]` (ptr
+                                    // to array of 10 ints)
+    // Suppress `-Werror=unused-variable` errors
+    (void)array2;
+    (void)array2_p;
+    (void)array2_p2;
+
+    // 2. Make some calls
+
+    // 2.1. calling with `int array1[5]`
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int *` (ptr to int); due to **natural type decay** from
+    // `int[5]` (array of 5 ints) to `int *` (ptr to int)
+    // my_func(array1);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int *` (ptr to int); due to dereferencing to `int[5]` (array
+    // of 5 ints), followed by **natural type decay** from `int[5]`
+    // (array of 5 ints) to `int *` (ptr to int)
+    // my_func(*array1_p2);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int *` (ptr to int)
+    // my_func(array1_p);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int`; due to **natural type decay** from `int[5]` (array of
+    // 5 ints) to `int *` (ptr to int), in conjunction with dereferencing
+    // from that to `int`
+    // my_func(*array1);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int`
+    // my_func(*array1_p);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int (*)[5]` (ptr to array of 5 ints)
+    // my_func(&array1);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int (*)[5]` (ptr to array of 5 ints)
+    // my_func(array1_p2);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int (**)[5]` (ptr to "ptr to array of 5 ints")
+    // my_func(&array1_p2);
+
+    // 2.2. calling with `int array2[10]`
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int *` (ptr to int); due to **natural type decay** from
+    // `int[10]` (array of 10 ints) to `int *` (ptr to int)
+    // my_func(array2);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int *` (ptr to int); due to dereferencing to `int[10]` (array
+    // of 10 ints), followed by **natural type decay** from `int[10]`
+    // (array of 10 ints) to `int *` (ptr to int)
+    // my_func(*array2_p2);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int *` (ptr to int)
+    // my_func(array2_p);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int`; due to **natural type decay** from `int[10]` (array of
+    // 10 ints) to `int *` (ptr to int), in conjunction with dereferencing
+    // from that to `int`
+    // my_func(*array2);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int`
+    // my_func(*array2_p);
+
+    // <===============
+    // <=== WORKS! ====
+    // <===============
+    // Expected and received `int (*)[10]` (ptr to array of 10 ints)
+    my_func(&array2);
+
+    // <===============
+    // <=== WORKS! ====
+    // <===============
+    // Expected and received `int (*)[10]` (ptr to array of 10 ints)
+    my_func(array2_p2);
+
+    // FAILS! Expected `int (*)[10]` (ptr to array of 10 ints) but argument is
+    // of type `int (**)[10]` (ptr to "ptr to array of 10 ints")
+    // my_func(&array2_p2);
+
+    // ===========================
+    printf("2D array tests: print a 2D array in a bunch of different ways:\n\n");
+    // ===========================
+
     int arr[][2] =
     {
         {1, 2},
@@ -280,10 +422,6 @@ int main()
     printf("NUM_ROWS(arr) = %zu\n", NUM_ROWS(arr));
     printf("NUM_COLS(arr) = %zu\n", NUM_COLS(arr));
     printf("\n");
-
-    // ===========================
-    printf("Print a 2D array in a bunch of different ways:\n\n");
-    // ===========================
 
     print_array1(arr, NUM_ROWS(arr), NUM_COLS(arr));
 
