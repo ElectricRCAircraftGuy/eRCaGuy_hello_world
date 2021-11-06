0dd11bb5bdfba58b45bd2713e10d989ac6f77869
Gabriel Staples <ercaguy@gmail.com>
2021-11-05 21:48:24 -0700

    wip: update pthreads_wikipedia_demo2.c

--- c/pthreads_practice/pthreads_wikipedia_demo2.c
+++ c/pthreads_practice/pthreads_wikipedia_demo2.c
@@ -27,7 +27,9 @@ References:
 // This is required in order for `syscall()` to be defined. See the example at the bottom of this
 // documentation here: https://man7.org/linux/man-pages/man2/syscall.2.html
 // and my comment here: https://stackoverflow.com/questions/21091000/how-to-get-thread-id-of-a-pthread-in-linux-c-program/21206357#comment122500554_32211287
+#ifndef __cplusplus
 #define _GNU_SOURCE
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -57,10 +59,11 @@ void *perform_work(void *arguments)
     sleep(sleep_time);
     printf("THREAD %d: Ended.\n", index);
 
-    // this is a bit of a hack to store an integer as though it was a pointer, but it works; it
-    // necessitates that I use these build flags, however:
-    // `-Wno-error=pointer-to-int-cast -Wno-error=int-to-pointer-cast`
-    return (void*)tid;
+    // this is a bit of a hack to store an integer as though it was a pointer, but it works; to
+    // avoid the error of: `error: cast to pointer from integer of different size
+    // [-Werror=int-to-pointer-cast]`, however, I first cast to an `int64_t` which is the same size
+    // as a ptr. I then do this in reverse later on, below, when reading this value back out.
+    return (void*)(int64_t)tid;
 }
 
 int main(void)
@@ -87,7 +90,7 @@ int main(void)
         void * retval;
         result_code = pthread_join(threads[i], &retval);
         assert(!result_code);
-        printf("IN MAIN: Thread %d (tid = %i) has ended.\n", i, (int)retval);
+        printf("IN MAIN: Thread %d (tid = %i) has ended.\n", i, (int)(int64_t)retval);
     }
 
     printf("MAIN program has ended.\n");
