9970ceb635041c1e90d0af0d33d59d73013cac98
Gabriel Staples <ercaguy@gmail.com>
2022-01-12 21:35:55 -0700

    bash/ping_until_alive.sh: add more URLs in comments for...
    ...what to work on next to implement this.

--- bash/ping_until_alive.sh
+++ bash/ping_until_alive.sh
@@ -1,1 +1,4 @@
 # ADD ME! See: https://unix.stackexchange.com/questions/678045/equivalent-of-ping-o-on-linux/678191#678191
+
+# See: eRCaGuy_dotfiles/useful_scripts/ping_loop.sh
+# [my ans] https://unix.stackexchange.com/questions/678045/equivalent-of-ping-o-on-linux/678191#678191
