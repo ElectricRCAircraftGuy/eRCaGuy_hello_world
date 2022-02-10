de2d0d44305fd54bb31a5e01eecc98f89e8ba7a0
Gabriel Staples <ercaguy@gmail.com>
2022-02-09 21:03:32 -0700

    Add bash/hello_world_basic.sh

---
+++ bash/hello_world_basic.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+# (description)
+
+# Run command:
+#       ./hello_world_basic.sh
+
+# References:
+# 1.
+
+
+echo "$@"
+
+
+
+# SAMPLE OUTPUT:
+#
+#       eRCaGuy_hello_world/bash$ ./hello_world_basic.sh a b c d e f g
+#       a b c d e f g
