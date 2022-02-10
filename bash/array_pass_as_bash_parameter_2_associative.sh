d708934ffd3102699485ad169c784cb18b80803c
Gabriel Staples <ercaguy@gmail.com>
2022-02-10 00:30:24 -0700

    bash/array_pass_as_bash_parameter_2_associative.sh: improve comments

--- bash/array_pass_as_bash_parameter_2_associative.sh
+++ bash/array_pass_as_bash_parameter_2_associative.sh
@@ -78,6 +78,9 @@ echo ""
 #       # Example
 #       print_associative_array2 array1
 print_associative_array2() {
+    # declare a local **reference variable** (hence `-n`) named `array_reference`
+    # which is a reference to the value stored in the first parameter
+    # passed in
     local -n array_reference="$1"
 
     # print the array by iterating through all of the keys now
