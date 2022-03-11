f7e1780f6826aa15595fb824d7b02fcb9504daef
Gabriel Staples <ercaguy@gmail.com>
2022-03-10 18:57:06 -0700

    Add python/hello_world.py

---
+++ python/hello_world.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+(description)
+Status: (status)
+
+keywords: (keywords)
+
+Check this script with: `shellcheck hello_world_basic.sh`
+
+Run command:
+```bash
+./hello_world.py
+# OR
+python3 hello_world.py
+```
+
+References:
+1.
+
+"""
+
+
+def main():
+    print("Hello world!")
+
+
+# Only run `main()` if this script is **run**, NOT imported
+if __name__ == '__main__':
+    main()
+
+
+
+"""
+SAMPLE OUTPUT:
+
+    eRCaGuy_hello_world/python$ ./hello_world.py
+    Hello world!
+
+"""
