ac537b135ba06c396b9d21d6bcb76a2b7360f5e0
Gabriel Staples <ercaguy@gmail.com>
2022-03-21 18:56:56 -0700

    Add cpp/TODO/remove_char_from_std_string_speedtest.cpp

---
+++ cpp/TODO/remove_char_from_std_string_speedtest.cpp
@@ -0,0 +1,19 @@
+/*
+GS
+Mar. 2022
+
+TODO: I still need to speed test and speed profile these ways to see which is fastest!
+
+C++ Remove new line from multiline string: https://stackoverflow.com/a/71565411/4561887
+- see my notes at the bottom of this, my answer:
+
+> Whats the most efficient way of removing a 'newline' from a `std::string`?
+
+As far as the _most efficient_ way goes--that I'd have to speed test/profile and see. I'll see if I
+can get back to you on that and run some speed tests between the top two answers here, and a
+C-style way like I did here: [Removing elements from array in C]
+(https://stackoverflow.com/a/70043744/4561887). I'll use [my `nanos()` timestamp function][2] for
+speed testing.
+
+
+*/
