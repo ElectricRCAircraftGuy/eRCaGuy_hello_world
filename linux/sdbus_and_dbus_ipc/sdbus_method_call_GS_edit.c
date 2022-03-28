e078cb4d5d44aa07bcd4888256e2e73a4f6df90c
Gabriel Staples <ercaguy@gmail.com>
2022-03-27 18:48:57 -0700

    sdbus_method_call_GS_edit.c: do minor cleanup

--- linux/sdbus_and_dbus_ipc/sdbus_method_call_GS_edit.c
+++ linux/sdbus_and_dbus_ipc/sdbus_method_call_GS_edit.c
@@ -53,38 +53,38 @@ int main(int argc, char *argv[]) {
         (void)argv;
 
         sd_bus_error error = SD_BUS_ERROR_NULL;
-        sd_bus_message *m = NULL;
+        sd_bus_message *message = NULL;
         sd_bus *bus = NULL;
         const char *path;
-        int r;
+        int retcode;
 
-        /* Connect to the system bus */
-        r = sd_bus_open_system(&bus);
-        if (r < 0) {
-                fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-r));
+        // Connect to the system bus
+        retcode = sd_bus_open_system(&bus);
+        if (retcode < 0) {
+                fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-retcode));
                 goto finish;
         }
 
-        /* Issue the method call and store the respons message in m */
-        r = sd_bus_call_method(bus,
-                               "org.freedesktop.systemd1",           /* service to contact */
-                               "/org/freedesktop/systemd1",          /* object path */
-                               "org.freedesktop.systemd1.Manager",   /* interface name */
-                               "StartUnit",                          /* method name */
-                               &error,                               /* object to return error in */
-                               &m,                                   /* return message on success */
-                               "ss",                                 /* input signature */
-                               "cups.service",                       /* first argument */
-                               "replace");                           /* second argument */
-        if (r < 0) {
+        // Issue the method call and store the response message in `message`
+        retcode = sd_bus_call_method(bus,
+                               "org.freedesktop.systemd1",           // service to contact
+                               "/org/freedesktop/systemd1",          // object path
+                               "org.freedesktop.systemd1.Manager",   // interface name
+                               "StartUnit",                          // method name
+                               &error,                               // object to return error in
+                               &message,                             // return message on success
+                               "ss",                                 // input signature
+                               "cups.service",                       // first argument
+                               "replace");                           // second argument
+        if (retcode < 0) {
                 fprintf(stderr, "Failed to issue method call: %s\n", error.message);
                 goto finish;
         }
 
-        /* Parse the response message */
-        r = sd_bus_message_read(m, "o", &path);
-        if (r < 0) {
-                fprintf(stderr, "Failed to parse response message: %s\n", strerror(-r));
+        // Parse the response message
+        retcode = sd_bus_message_read(message, "o", &path);
+        if (retcode < 0) {
+                fprintf(stderr, "Failed to parse response message: %s\n", strerror(-retcode));
                 goto finish;
         }
 
@@ -92,10 +92,10 @@ int main(int argc, char *argv[]) {
 
 finish:
         sd_bus_error_free(&error);
-        sd_bus_message_unref(m);
+        sd_bus_message_unref(message);
         sd_bus_unref(bus);
 
-        return r < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+        return retcode < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 /*
