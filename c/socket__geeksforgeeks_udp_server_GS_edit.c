5d0d0577fea1c0f721ebfc049b6efee0579efa99
Gabriel Staples <ercaguy@gmail.com>
2022-03-31 21:27:31 -0700

    socket__geeksforgeeks_udp_server_GS_edit.c: run clang-format
        clang-format --verbose -i --style=file socket__geeksforgeeks_udp_server_GS_edit.c

--- c/socket__geeksforgeeks_udp_server_GS_edit.c
+++ c/socket__geeksforgeeks_udp_server_GS_edit.c
@@ -32,27 +32,29 @@ References:
 
 // Server-side implementation of UDP server-client model
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define PORT     8080
+#define PORT 8080
 #define MAXLINE 1024
 
 // Driver code
-int main() {
+int main()
+{
     int sockfd;
     char buffer[MAXLINE];
     char *hello = "Hello from server";
     struct sockaddr_in servaddr, cliaddr;
 
     // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -61,13 +63,12 @@ int main() {
     memset(&cliaddr, 0, sizeof(cliaddr));
 
     // Filling server information
-    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_family = AF_INET;  // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
 
     // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,
-            sizeof(servaddr)) < 0 )
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -75,36 +76,33 @@ int main() {
 
     int len, n;
 
-    len = sizeof(cliaddr); //len is value/resuslt
+    len = sizeof(cliaddr);  // len is value/resuslt
 
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-                &len);
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
     buffer[n] = '\0';
     printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-            len);
+    sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM,
+           (const struct sockaddr *)&cliaddr, len);
     printf("Hello message sent.\n");
 
     return 0;
 }
-
 
 /*
 SAMPLE OUTPUT:
 
 In C:
 
-    eRCaGuy_hello_world/c$ gcc -Wall -Wextra -O3 -std=c17 socket__geeksforgeeks_udp_server_GS_edit.c -o bin/server -lm && bin/server
-    socket__geeksforgeeks_udp_server_GS_edit.c: In function ‘main’:
-    socket__geeksforgeeks_udp_server_GS_edit.c:73:17: warning: pointer targets in passing argument 6 of ‘recvfrom’ differ in signedness [-Wpointer-sign]
-                     &len);
+    eRCaGuy_hello_world/c$ gcc -Wall -Wextra -O3 -std=c17 socket__geeksforgeeks_udp_server_GS_edit.c
+-o bin/server -lm && bin/server socket__geeksforgeeks_udp_server_GS_edit.c: In function ‘main’:
+    socket__geeksforgeeks_udp_server_GS_edit.c:73:17: warning: pointer targets in passing argument 6
+of ‘recvfrom’ differ in signedness [-Wpointer-sign] &len);
                      ^~~~
     In file included from /usr/include/x86_64-linux-gnu/sys/socket.h:269,
                      from socket__geeksforgeeks_udp_server_GS_edit.c:31:
-    /usr/include/x86_64-linux-gnu/bits/socket2.h:64:1: note: expected ‘socklen_t * restrict’ {aka ‘unsigned int * restrict’} but argument is of type ‘int *’
-     recvfrom (int __fd, void *__restrict __buf, size_t __n, int __flags,
+    /usr/include/x86_64-linux-gnu/bits/socket2.h:64:1: note: expected ‘socklen_t * restrict’ {aka
+‘unsigned int * restrict’} but argument is of type ‘int *’ recvfrom (int __fd, void *__restrict
+__buf, size_t __n, int __flags,
      ^~~~~~~~
 
     Client : Hello from client
@@ -114,8 +112,8 @@ In C:
 
 OR, in C++:
 
-    eRCaGuy_hello_world/c$ g++ -Wall -Wextra -Werror -O3 -std=c++17 socket__geeksforgeeks_udp_server_GS_edit.c -o bin/a && bin/a
-    Hello World.
+    eRCaGuy_hello_world/c$ g++ -Wall -Wextra -Werror -O3 -std=c++17
+socket__geeksforgeeks_udp_server_GS_edit.c -o bin/a && bin/a Hello World.
 
 
 */
