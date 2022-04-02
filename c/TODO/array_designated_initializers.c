8bbf9d46d4c87e0d17fd2c0f49e7cc9e6e34dea7
Gabriel Staples <ercaguy@gmail.com>
2022-04-01 22:17:51 -0700

    Add TODO/array_designated_initializers.c

---
+++ c/TODO/array_designated_initializers.c
@@ -0,0 +1,9 @@
+/*
+See:
+https://stackoverflow.com/questions/9849719/what-do-square-brackets-mean-in-array-initialization-in-c/9849782#9849782
+https://gcc.gnu.org/onlinedocs/gcc-4.0.4/gcc/Designated-Inits.html
+
+be sure to also do demo which shows whether uninitialized indices are initialized to zero.
+See: https://stackoverflow.com/questions/9849719/what-do-square-brackets-mean-in-array-initialization-in-c/9849782#comment125282837_9849782
+
+*/
