e9101c6fde821f0ac20bab9f3661953147899905
Gabriel Staples <ercaguy@gmail.com>
2022-04-09 14:26:47 -0700

    Finish python/hello_george.py

--- python/hello_george.py
+++ python/hello_george.py
@@ -4,31 +4,33 @@
 This file is part of eRCaGuy_hello_world:
 https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
 
-By Gabriel Staples and George
+Code By Gabriel Staples and George Epperson
 Written 11 March 2022 12:21:56 PM MST
 
 Description:
+By Gabriel Staples
+Written 8-9 Apr. 2022
 Print "Hey George", with the index number first, 10 times.
 
-Contributed to my code repository 8 Apr. 2022 in memory of my wife's grandpa, George, who married
+Contributed to my code repository 9 Apr. 2022 in memory of my wife's grandpa, George, who married
 and sealed us. He died 23 March 2022, just 12 days after he and I wrote this code together. At the
 time he died he was 88 years old, just 2 days before his 89th birthday. We went to visit him in Los
 Alamos, NM, and he asked what I do. He knows virtually nothing about computers and technology, not
-even what the internet is really nor what a router is. He can hardly use email with assistance, and
+even what the internet is really, nor what a router is. He can hardly use email with assistance, and
 he still has a flip phone.
 
 I talked briefly about programming and said, "You know what! Let me just show you." He and I then
 sat down together in some chairs in his master bedroom, with him in a white button up dress shirt
-and nice jeans, and on oxygen with tubes in his nose, and we wrote this code together and I talked
+and nice jeans, on oxygen with tubes in his nose, and we wrote this code together while I talked
 about programming, "for" loops, and how all computers and technology are built up in chunks of code
-and building blocks, just like nails and boards in a house. I said, "the problem with programming
-and software architecture, however, is that sometimes when building your house of software, you
-need a nail that doesn't exist yet, and you have to pause to go make the 'nail'. But then you
-realize you need the raw iron to make the nail, and a forge to melt the iron to make the nail, and
-the wood to heat it, and you need to go find, borrow, or make all of those things too. Something
-that seems so simple on the outside to the untrained observer can take weeks or months or more to
-build up a little at a time in code." He was in the construction business in his life, so I think
-he could understand the analogy of building software to building a house.
+and building blocks, just like nails and boards and other pieces in a house. I said, "the problem
+with programming and software architecture, however, is that sometimes when building your house of
+software, you need a nail that doesn't exist yet, and you have to pause to go make the 'nail'. But
+then you realize you need the raw iron to make the nail, and a forge to melt the iron to make the
+nail, and the wood to heat it, and you need to go find, borrow, or make all of those things too.
+Something that seems so simple on the outside to the untrained observer can take weeks or months or
+more to build up a little at a time in code." He was in the construction business in his life, so I
+think he could understand the analogy of building software to building a house.
 
 His mind was blown. He asked if I thought self-driving cars were really coming (I work as a senior
 embedded software engineer on self-driving cars at Cruise right now; see:
@@ -39,24 +41,148 @@ mean all self-driving cars won't have a steering wheel. I think most cars will c
 steering wheel, at least for the next 100 years or so, to allow drivers to drive them manually when
 they want to feel in control, or when they want to go off-roading or just have fun with them, but
 most if not all cars will be self-driving within 100 years." Again, his mind was blown. He said he
-didn't think he'd get in one. We agreed that some people would just need to die off to make this
-happen, as older generations passing away is all part of the process of progression.
+didn't think he'd get in one. Referring to peoples' natural resistance to change as we grow older,
+we agreed that older generations passing away is frequently part of the process of progression
+which welcomes and allows drastic changes and progress like this to happen.
 
-I mentioned that I saw a presentation once at AFRL (Air Force Research Laboratory) that when the
-automobile was first invented, they were limited to a max speed of 5 mph in New York City, and a
-flag person had to walk before the automobile saying, "An automobile is coming! An automobile is
-coming." A second flag bearer had to walk behind the car as well, for "safety", to protect the
+I mentioned that I saw a presentation once at AFRL (Air Force Research Laboratory) in ~2013 that
+when the automobile was first invented, they were limited to a max speed of 5 mph in New York City,
+and a flag person had to walk before the automobile saying, "An automobile is coming! An automobile
+is coming." A second flag bearer had to walk behind the car as well, for "safety", to protect the
 horses and buggies from this unruly beast of a machine which certainly could not be trusted because
-it didn't eat nor drink nor have a mind of its own. It was **dangerous!** Anyway, similar opinions
-and inhibitions sometimes exist about the self-driving car today. But, they will pass. Electric
-charge stations will be build. Infrastructure will be built, and even in 30 years self-driving cars
-will be fairly common-place in many large cities throughout the world.
+it didn't eat nor drink nor have a mind of its own. It was **dangerous** because it was new and did
+not have a conscience, self-awareness, and a brain! Anyway, similar opinions and inhibitions
+sometimes exist about the self-driving car today. But, they will pass. Electric charge stations
+will be build. Infrastructure will be built, and even in 30 years self-driving cars will be fairly
+common-place in many large cities throughout the world.
 
-Grandpa George said he was ready to pass away, and looked forward to dying. He wanted to be with his
+Grandpa George said he was looking forward to seeing his wife again, who had died years earlier. He
+was ready to go, and was looking forward to passing on to the next life. He wanted to be with his
 wife again. He was ready to go. I put my computer away and we had a nice visit. We had printed "Hey
-George" 10 times to the computer screen in Python. May he rest in peace and find love in the arms
-of his wife, and his Savior, Jesus Christ, who he probably has not seen yet, but certainly will see
-at some point in the future.
+George" 10 times to the computer screen in Python.
+
+May he rest in peace and find love in the arms of his wife, and his Savior, Jesus Christ.
+
+So, what happens after death?
+
+I believe the following:
+
+I believe George probably has not seen Jesus Christ yet, but certainly will see Him at some point in
+the future. When we die, our spirits separate from our bodies. Our spirits, and our conscience,
+which is what makes us "us", go to the Spirit World, either in a state of happiness called Spirit
+Paradise, or in a state of misery called Spirit Prison. People living in Spirit Paradise can teach
+the Gospel of Jesus Christ to those in Spirit Prison. There is much work to do. It is not an idle
+place, but it *is* a place of rest from physical bodily cares and needs and worries and pains--just
+a very busy place of rest where there is much work to do. You will *not* instantly see Christ. You
+will *not* instantly be changed to anything other than what you were at the moment you died. Your
+mind will be with you, and your personality, mind, thoughts, knowledge, beliefs, etc., will be the
+same in the Spirit World as they were when you died, or as they *would have been* when you died had
+you been in a healthy physical body (in the case of mental degeneration or other physical
+impairment or handicap). In other words, you will still be "you." Your knowledge and personality
+will still be with you. Engineers and scientists will still understand the truths of engineering
+and science. Programmers will still understand and follow the truth of computers, computation,
+electronics, physics, and electrical engineering. Non-Christians will still be non-Christian.
+Stubborn people will still be stubborn. People who swear and use foul language will still swear and
+use foul language. People who desire to do evil and to harm one another or abuse each other will
+still desire to do these evil things. Lazy people will still be lazy. Hard workers will still be
+hard workers. Drug addicts will still be drug addicts, except they will be in constant torment
+because they no longer have a physical body to take drugs to satisfy their cravings. Other
+addictions will still be present, except they also will not be able to be satisfied if a physical
+body is required to satisfy them. This includes sex addictions, alcohol, tobacco, etc.
+
+Anyway, George is still George, except now I think he gets to see his wife. After some time in the
+Spirit World, working to teach people the Gospel of Jesus Christ, he will be brought before Christ
+to be judged of his works, thoughts, actions, attitudes, beliefs, etc., as will *all* people in the
+Spirit World. At that moment there will be no doubt for him nor for anyone else who stands before
+Jesus that Jesus is the Christ, the Son of the Living God, the Savior and Redeemer of the world.
+
+That is the moment that the scriptures talk of when they say:
+    > For it is written, As I live, saith the Lord, every knee shall bow to me, and every tongue
+      shall confess to God. So then every one of us shall give account of himself to God.
+      (Romans 14:11-12, New Testament:
+      https://abn.churchofjesuschrist.org/study/scriptures/nt/rom/14?lang=eng)
+
+And:
+    > 31 Yea, every knee shall bow, and every tongue confess before him. Yea, even at the last day,
+      when all men shall stand to be judged of him, then shall they confess that he is God; then
+      shall they confess, who live without God in the world, that the judgment of an everlasting
+      punishment is just upon them; and they shall quake, and tremble, and shrink beneath the
+      glance of his all-searching eye.
+      (Mosiah 27:31, Book of Mormon:
+      https://abn.churchofjesuschrist.org/study/scriptures/bofm/mosiah/27?lang=eng)
+
+Anyway, I believe George to be in Spirit Paradise in a state of happiness and physical rest until
+his judgement and resurrection. But, he's got much work to do.
+
+From The Plan of Salvation pamphlet of the
+Church of Jesus Christ of Latter-day Saints, p10-12,
+https://media.ldscdn.org/pdf/manuals/the-plan-of-salvation/36950-the-plan-of-salvation-eng.pdf?lang=eng:
+
+    > At death, your spirit will leave your body and go to the spirit world, which is a place of
+      learning and preparation. In the spirit world, your memories of this life will remain with
+      you. Death will not change your personality or your desire for good or evil. If you choose to
+      follow Christ during your life on earth, you will be at peace in the spirit world and will
+      rest from your cares. Those who choose not to follow Jesus Christ and do not repent will be
+      unhappy.
+    > ...
+    > Resurrection and Judgment:
+    > One of the great gifts of God to all who come to earth is the resurrection, which is made
+      possible by the Atonement of Jesus Christ. When Jesus died on the cross, His spirit went to
+      the spirit world. Three days later, His spirit was reunited with His glorified, perfected
+      body that could no longer die. This reuniting of body and spirit is called resurrection.
+      Everyone who is born on earth will be resurrected. After you are resurrected, you will go
+      before God to be judged according to your works and the desires of your heart.
+
+    > Degrees of Glory:
+
+    > After you are judged, you will live in a state of glory. Because everyone’s works and desires
+      vary, heaven includes different kingdoms, or degrees of glory.
+
+    > **Celestial kingdom.** Our Heavenly Father and Jesus Christ reside in the celestial kingdom.
+      If you live according to the gospel of Jesus Christ and are cleansed from sin by the
+      Atonement, you will receive a place in this, the highest kingdom. You will live in God’s
+      presence and know complete joy.
+    > **Terrestrial kingdom.** People who refuse to accept the gospel of Jesus Christ but who live
+      honorable lives will receive a place in the terrestrial kingdom.
+    > **Telestial kingdom.** Those who continue in their sins and do not repent will receive a place
+      in the telestial kingdom.
+
+
+This is a summary diagram of this timeline of events we call "The Plan of Salvation". I believe
+these things.
+Sources:
+1. I drew this with https://asciiflow.com/
+2. Full edit link to this diagram:
+   https://asciiflow.com/#/share/eJzVVs1q20AQfpVBp4aqhjpuaV16aBzHIfRgkkAvuqylsbVltSt213ZMCJQ%2Bg8mD9Fj6NH6Szkr%2BjwS2ayV0WCOtNbMz%2BvTtt3PvSZag1%2FRuY4SuYBJUH26YGDHLlfR8T7AJanp%2BH3h3gdf82Gj4gTehu%2FqH93Rn8c7SJPDg2e2VSbnmFnoqmsBr4EmitGUC0nhieEg37kEQyOevDD6D4CMuB%2F6yKh80mqHWGFqMwKihOKmytHVsPlWKwRiBaYRxrLJr9pNR1Sl7KDiOslTAQgvMPPF6W4MWCjSWM1F1ORGPQEloM23jk2Kv2fTP7uNlWLtm9RrcIrHVWF01fLvYnshtsL900S39KFKP7RDi1E0e1GWaRdzglsOpw22DdMQO0gLa82Nu49JaFoKRK8NxIVkfhy2Sv0lHRTtVtp%2FVV5BqbpSskmuHYfYy7L%2FgkkjodqCjRSpYiMAlXCIboWxmNXU1vkEnOfCV97eZuG7tXZyWNv8c35QW0eYTyq8kug7BUrdwCuc40IjG%2FdERSrvDdjb9ceyx7wc7egH%2F4ziE6Bvj8XcJ72ePv8pmtINlyFORU8JxpKNMiqJZFl2yflnm6c%2ByGQnzBXMCSxS9okbLQCsmObGl0SXr77fXSbuuMUVpmQyxILSlMWujfTfpLg6WzOigOGOp5SYpWHfpek7xRYfG9aKX5IWCOQd%2FbmdcrxZp1KDD%2B3bhcqnEBDqxypFy3RSX3B1exS%2FsHDJhKkg6lHTMaYP%2B07B3NWjLaEitoVVZWpQrZfk%2BjAYJYVickexq4dDDvtJFOGdlMVFS9D9YTqJihL9ELCmppT3CwHvwHv4CjEz7Lw%3D%3D)
+
+
+
+                                                                                                                  (spirit body + immortal physical body
+                                                                                                                   = living, immortal, resurrected soul)
+                                                                                         (spirit body;
+                                                                                          we are who are are and
+                                                                                          we believe and act as                 1. Celestial
+                                                                                          we did on Earth)                      ───────────────
+                                                                                                                              2. Terrestrial
+                                                                                                                              ──────────────
+  (spirit body;                         (spirit body + mortal physical body                1. Spirit Paradise               3. Telestial
+   we lived with                         = living soul)                                    ──────────────────               ─────────────
+   God)                                                                                  2. Spirit Prison
+                                                                                         ──────────────────
+                                                                                                                   Final resting place in Heaven:
+  Pre-earth Life                           Earth Life                                    Spirit World              in one of the 3 Degrees of Glory
+────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────►
+                    ▲           ▲          Principles of the Gospel:          ▲                               ▲
+                    │           │          1. Faith in Jesus Christ           │                               │
+                                           2. Repentance
+                Creation,    Physical      3. Baptism                   Physical Death                   Resurrection
+                of the        Birth        4. Gift of the Holy Ghost    and initial                      and Final
+                universe,                  5. Endure to the end         judgment                         Judgment before
+                and Fall                                                                                 Christ
+                of Adam
+                and Eve
+
+
 
 Enjoy watching "Hey George" print 10 times.
 
@@ -73,7 +199,7 @@ python3 hello_george.py
 ```
 
 
-References:
+Programming References:
 1. Python f-strings / f-format strings: https://realpython.com/python-f-strings/
 
 """
