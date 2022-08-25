4822575b873872fac1a9a3c67ca74dc5ab890171
Gabriel Staples <ercaguy@gmail.com>
2022-08-25 00:32:52 -0700

    Finish cpp/yaml-cpp_lib_demo.cpp!
    The demo now works and is done! It shows how to
    iterate over yaml sequences and maps too.

--- cpp/yaml-cpp_lib_demo.cpp
+++ cpp/yaml-cpp_lib_demo.cpp
@@ -7,7 +7,7 @@ Aug. 2022
 Learn how to link to and use the `yaml-cpp` library (https://github.com/jbeder/yaml-cpp).
 See full installation instructions to use this library, in "eRCaGuy_hello_world/cpp/README.md".
 
-STATUS: wip
+STATUS: done and works!
 
 
 Build commands:
@@ -20,6 +20,7 @@ Build commands:
 # "eRCaGuy_hello_world/cpp/README.md"
 # THEN, use one of the two options below to build and run this code:
 
+# [my preference]
 # Option 1: build from the static library which is **installed to the system** by specifying
 # `-lyaml-cpp`
 time g++ -Wall -Wextra -Werror -O3 -std=c++17 yaml-cpp_lib_demo.cpp -o bin/a -lyaml-cpp && bin/a
@@ -49,6 +50,7 @@ References:
 // NA
 
 // C & C++ includes
+#include <cassert>
 #include <cstdint>  // For `uint8_t`, `int8_t`, etc.
 #include <cstdio>   // For `printf()`
 #include <iostream>  // For `std::cin`, `std::cout`, `std::endl`, etc.
@@ -59,8 +61,10 @@ int main()
     printf("Practice ");
     std::cout << "reading from a YAML file!\n\n";
 
-    YAML::Node yaml_config;
+    YAML::Node root_node;
+    const char* filename;
     constexpr char YAML_FILE_PATH_WRONG[] = "yaml-cpp_lib_demooo.yaml";
+    constexpr char YAML_FILE_PATH_EMPTY[] = "yaml-cpp_lib_demo_empty.yaml";
     constexpr char YAML_FILE_PATH[] = "yaml-cpp_lib_demo.yaml";
 
     // Show how to catch a thrown exception by intentionally typing the file path wrong so that
@@ -71,13 +75,12 @@ int main()
     // 1. eRCaGuy_hello_world/cpp/yaml-cpp/src/parse.cpp
     try
     {
-        yaml_config = YAML::LoadFile(YAML_FILE_PATH_WRONG);
+        root_node = YAML::LoadFile(YAML_FILE_PATH_WRONG);
     }
     catch (const std::exception& e)
     {
-        printf("Unable to load file \"%s\".\n"
-               "  Exception caught: e.what() = ", YAML_FILE_PATH_WRONG);
-        std::cout << e.what() << "\n";
+        std::cout << "Unable to load file \"" << YAML_FILE_PATH_WRONG << "\".\n"
+                  << "  Exception caught: e.what() = " << e.what() << "\n";
     }
     std::cout << "Trying again...\n";
 
@@ -85,14 +88,84 @@ int main()
     //      terminate called after throwing an instance of 'YAML::BadFile'
     //        what():  bad file: yaml-cpp_lib_demooo.yaml
     //      Aborted (core dumped)
-    // yaml_config = YAML::LoadFile(YAML_FILE_PATH_WRONG);
+    // root_node = YAML::LoadFile(YAML_FILE_PATH_WRONG);
 
-    // Now do it correctly
-    YAML::Node config = YAML::LoadFile(YAML_FILE_PATH);
+    // Now load a file which *does* exist, but is empty.
+    filename = YAML_FILE_PATH_EMPTY;
+    root_node = YAML::LoadFile(filename);
+    if (root_node.IsNull())
+    {
+        std::cout << "Error: YAML::Node is Null, indicating the yaml file was empty or invalid.\n"
+                     "  Filename = \"" << filename << "\".\n";
+    }
+    else
+    {
+        std::cout << "YAML file successfully loaded.\n";
+    }
 
+    // Finally, load a good and valid file!
+    filename = YAML_FILE_PATH;
+    root_node = YAML::LoadFile(filename);
+    if (root_node.IsNull())
+    {
+        std::cout << "Error: YAML::Node is Null, indicating the yaml file was empty or invalid.\n"
+                     "  Filename = \"" << filename << "\".\n";
+    }
+    else
+    {
+        std::cout << "YAML file \"" << filename << "\" successfully loaded.\n";
+    }
 
+    // Print out the whole thing!
+    std::cout << "\nWHOLE FILE:\n" << root_node << "\n\n";
 
+    // Extract just some parts I care about. See the tutorial:
+    // https://github.com/jbeder/yaml-cpp/wiki/Tutorial
 
+    assert(root_node.IsMap());
+    assert(root_node["data"].IsSequence());
+
+    // Iterate over the "data" sequence (list) in this yaml file, printing select data
+    YAML::Node data_node = root_node["data"];
+    assert(data_node.IsSequence());
+    for (size_t i = 0; i < data_node.size(); i++)
+    {
+        std::cout << data_node[i]["name"] << ", " << data_node[i]["value"] << "\n";
+    }
+
+    // Iterate over the "data" sequence (list) in this yaml file, printing each map in the list
+    std::cout << "\n";
+    for (size_t i = 0; i < data_node.size(); i++)
+    {
+        std::cout << "data_node[" << i << "] = \n" << data_node[i] << "\n\n";
+    }
+
+    // Iterate over a map in the data
+    YAML::Node data_phi = root_node["data"][6];
+    assert(data_phi.IsMap());
+    std::cout << "data_phi = \n" << data_phi << "\n\n";
+    std::cout << "data_phi.size() = " << data_phi.size() << "\n";
+    // NB: this does NOT work! See the output. This is how you iterate over a sequence, NOT a map!
+    for (size_t i = 0; i < data_phi.size(); i++)
+    {
+        // Does not work right. That's the point: to show you what NOT to do. :)
+        std::cout << "data_phi[" << i << "] = " << data_phi[i] << "\n";
+    }
+    std::cout << "\n";
+    // This is correct to iterate over a map!
+    // See the `YAML::Node lineup` example here:
+    // https://github.com/jbeder/yaml-cpp/wiki/Tutorial#basic-parsing-and-node-editing
+    for (YAML::const_iterator it = data_phi.begin(); it != data_phi.end(); it++)
+    {
+        std::string key = it->first.as<std::string>();
+        std::string value = it->second.as<std::string>();
+        printf("key = %-15s value = %-15s\n", key.c_str(), value.c_str());
+    }
+    std::cout << "\n";
+
+    // Do some math on a value from a map node
+    double value = data_phi["value"].as<double>();
+    printf("phi value = %f; phi value*2.1 = %f\n", value, value*2.1);
 
 
     return 0;
@@ -103,6 +176,176 @@ int main()
 /*
 SAMPLE OUTPUT:
 
+    eRCaGuy_hello_world/cpp$ time g++ -Wall -Wextra -Werror -O3 -std=c++17 yaml-cpp_lib_demo.cpp -o bin/a -lyaml-cpp && bin/a
+
+    real    0m2.449s
+    user    0m2.240s
+    sys 0m0.130s
+    Practice reading from a YAML file!
+
+    Unable to load file "yaml-cpp_lib_demooo.yaml".
+      Exception caught: e.what() = bad file: yaml-cpp_lib_demooo.yaml
+    Trying again...
+    Error: YAML::Node is Null, indicating the yaml file was empty or invalid.
+      Filename = "yaml-cpp_lib_demo_empty.yaml".
+    YAML file "yaml-cpp_lib_demo.yaml" successfully loaded.
+
+    WHOLE FILE:
+    name: yaml-cpp_demo.yaml
+    description: This is a demo yaml file that can be imported, for example, by the yaml-cpp library.
+    author: Gabriel Staples
+    date: Aug. 2022
+    data:
+      - name: x
+        description: x position
+        units: mm
+        type: int64_t
+        value: 100
+      - name: y
+        description: y position
+        units: mm
+        type: int64_t
+        value: 200
+      - name: z
+        description: z position
+        units: mm
+        type: int64_t
+        value: 300
+      - name: u
+        description: velocity in the x direction
+        units: mm/s
+        type: int64_t
+        value: 10
+      - name: v
+        description: velocity in the y direction
+        units: mm/s
+        type: int64_t
+        value: 11
+      - name: w
+        description: velocity in the z direction
+        units: mm/s
+        type: int64_t
+        value: 12
+      - &1
+        name: phi
+        description: roll angle
+        units: deg
+        type: double
+        value: 0.10
+      - name: theta
+        description: pitch angle
+        units: deg
+        type: double
+        value: -0.20
+      - name: psi
+        description: yaw angle (heading)
+        units: deg
+        type: double
+        value: 45.1234
+      - *1
+
+    x, 100
+    y, 200
+    z, 300
+    u, 10
+    v, 11
+    w, 12
+    phi, 0.10
+    theta, -0.20
+    psi, 45.1234
+    phi, 0.10
+
+    data_node[0] =
+    name: x
+    description: x position
+    units: mm
+    type: int64_t
+    value: 100
+
+    data_node[1] =
+    name: y
+    description: y position
+    units: mm
+    type: int64_t
+    value: 200
+
+    data_node[2] =
+    name: z
+    description: z position
+    units: mm
+    type: int64_t
+    value: 300
+
+    data_node[3] =
+    name: u
+    description: velocity in the x direction
+    units: mm/s
+    type: int64_t
+    value: 10
+
+    data_node[4] =
+    name: v
+    description: velocity in the y direction
+    units: mm/s
+    type: int64_t
+    value: 11
+
+    data_node[5] =
+    name: w
+    description: velocity in the z direction
+    units: mm/s
+    type: int64_t
+    value: 12
+
+    data_node[6] =
+    name: phi
+    description: roll angle
+    units: deg
+    type: double
+    value: 0.10
+
+    data_node[7] =
+    name: theta
+    description: pitch angle
+    units: deg
+    type: double
+    value: -0.20
+
+    data_node[8] =
+    name: psi
+    description: yaw angle (heading)
+    units: deg
+    type: double
+    value: 45.1234
+
+    data_node[9] =
+    name: phi
+    description: roll angle
+    units: deg
+    type: double
+    value: 0.10
+
+    data_phi =
+    name: phi
+    description: roll angle
+    units: deg
+    type: double
+    value: 0.10
+
+    data_phi.size() = 5
+    data_phi[0] =
+    data_phi[1] =
+    data_phi[2] =
+    data_phi[3] =
+    data_phi[4] =
+
+    key = name            value = phi
+    key = description     value = roll angle
+    key = units           value = deg
+    key = type            value = double
+    key = value           value = 0.10
+
+    phi value = 0.100000; phi value*2.1 = 0.210000
 
 
 */
