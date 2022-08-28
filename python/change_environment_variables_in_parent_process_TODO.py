fa7f3b8368b9a24fbad21568b6bc58a10d7b95b0
Gabriel Staples <ercaguy@gmail.com>
2022-08-27 22:08:24 -0700

    Add python/change_environment_variables_in_parent_process_TODO.py

---
+++ python/change_environment_variables_in_parent_process_TODO.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""
+This file is part of eRCaGuy_hello_world:
+https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+TODO: apparently I thought I needed a demo sometime in 2021 or 2022 to change environment variables
+in a parent process. I'm putting this file on-hold and marking it "*_TODO" in the filename.
+
+Status: NOT STARTED
+
+keywords: (keywords)
+
+Check this script with `pylint` v2.0.0 or later. See "eRCaGuy_hello_world/python/README.md" for
+installation instructions to install the latest version from GitHub.
+For a list of all error codes, such as `C0301`, `C0116`, `W0105`, etc., see here:
+https://pylint.pycqa.org/en/latest/messages/messages_list.html
+```bash
+pylint hello_world.py
+```
+
+Run command:
+```bash
+./hello_world.py
+# OR
+python3 hello_world.py
+```
+
+References:
+1.
+
+"""
+
+
+def main():
+    """
+    The main function of this program.
+    """
+    print("Hello world!")
+
+
+# Only run `main()` if this script is **run**, NOT imported
+if __name__ == '__main__':
+    main()
+
+
+
+# pylint: disable-next=pointless-string-statement
+"""
+SAMPLE OUTPUT:
+
+    eRCaGuy_hello_world/python$ ./hello_world.py
+    Hello world!
+
+"""
