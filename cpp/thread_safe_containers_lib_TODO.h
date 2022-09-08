a0354a0f49c442f73008dbaeee0ec8996739c716
Gabriel Staples <ercaguy@gmail.com>
2022-09-07 23:25:14 -0700

    Add cpp/thread_safe_containers_lib_TODO.h

---
+++ cpp/thread_safe_containers_lib_TODO.h
@@ -0,0 +1,14 @@
+/*
+This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+Add code for thread-safe queues, deques, vectors, maps, etc. (template it
+for *any* type), to safely share data between multiple threads. This will
+simply require a basic mutex.
+
+Include mechanisms for basic thread synchronization and notifying too, by waking
+up threads to notify them there is data for them to read, via
+`std::condition_variable`s and all.
+
+See: https://gabrielstaples.com/cpp-mutexes-and-locks/
+
+*/
