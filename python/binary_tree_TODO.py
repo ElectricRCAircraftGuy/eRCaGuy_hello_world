2486551af55ba180c1edc7eb9cb46a9b81d86808
Gabriel Staples <ercaguy@gmail.com>
2022-12-08 21:22:52 -0700

    Add some python TODOs to learn how to make and traverse binary trees
    	new file:   python/binary_tree_TODO.py
    	new file:   python/binary_tree_traverse_and_print_TODO.py

---
+++ python/binary_tree_TODO.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+This file is part of eRCaGuy_hello_world:
+https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+See here for starters:
+1. https://stackoverflow.com/q/74738381/4561887
+    1. Google search for "how to make a binary tree in python":
+    https://www.google.com/search?q=how+to+make+a+binary+tree+in+python&oq=how+to+make+a+binary+tree+in+python&aqs=chrome..69i57.7738j0j7&client=ms-android-google&sourceid=chrome-mobile&ie=UTF-8#sbfbu=1&pi=how%20to%20make%20a%20binary%20tree%20in%20python
+
+
+(description)
+Status: (status)
+
+keywords: (keywords)
+
+Check this script with `pylint` v2.0.0 or later. See "eRCaGuy_hello_world/python/README.md" for
+installation instructions to install the latest version from GitHub.
+For a list of all error codes, such as `C0301`, `C0116`, `W0105`, etc., see here:
+https://pylint.pycqa.org/en/latest/messages/messages_list.html
+```bash
+pylint hello_world.py
+```
+
+Run command:
+```bash
+./hello_world.py
+# OR
+python3 hello_world.py
+```
+
+References:
+1.
+
+"""
+
+
+def main():
+    """
+    The main function of this program.
+    """
+    print("Hello world!")
+
+
+# Only run `main()` if this script is **run**, NOT imported
+if __name__ == '__main__':
+    main()
+
+
+
+# pylint: disable-next=pointless-string-statement
+"""
+SAMPLE OUTPUT:
+
+    eRCaGuy_hello_world/python$ ./hello_world.py
+    Hello world!
+
+"""
