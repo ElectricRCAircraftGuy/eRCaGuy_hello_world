0ec4e39793801a7e35e65be0946fd482c9ebb5e9
Gabriel Staples <ercaguy@gmail.com>
2022-12-28 22:35:58 -0700

    Update python/socket_udp_send_packed_struct_TODO.py
    ...with more placeholder code and initial bits.

--- python/socket_udp_send_packed_struct_TODO.py
+++ python/socket_udp_send_packed_struct_TODO.py
@@ -7,6 +7,10 @@ https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
 27 Aug. 2022
 
 Practice packing a binary struct and sending it out over an Ethernet UDP socket!
+- Start by learning how the `struct` modules works in Python, to pack and unpack C-like structs.
+  See:
+  1. eRCaGuy_hello_world/python/struct_packed.py
+  1. https://docs.python.org/3/library/struct.html
 
 Status: wip
 
@@ -34,11 +38,32 @@ References:
 """
 
 
+import socket
+import struct
+
+
+def udp_send():
+    print("Sending a packed struct over UDP.")
+
+    # 1. Open a UDP socket to send UDP Ethernet packets
+
+    # A UDP socket is a "Datagram" type (SOCK_DGRAM) over "Address Family: Internet" (AF_INET)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UDP_TARGET = ("192.168.0.2", 10000)  # IP and PORT that we will send a packed struct to
+
+    # 2. Create a packed struct of data to send
+
+    # 3. send the packed struct
+
+
+def udp_listen():
+    pass
+
+
 def main():
     """
     The main function of this program.
     """
-    print("Hello world!")
 
 
 # Only run `main()` if this script is **run**, NOT imported
