0ac85af88aaaa391f5f91cfe0f355884c0a15247
Gabriel Staples <ercaguy@gmail.com>
2022-12-28 22:51:12 -0700

    Add cpp/binary_tree_comparison_depth1stSearch_vs_breadth1stSearch_TODO.cpp placeholder

---
+++ cpp/binary_tree_comparison_depth1stSearch_vs_breadth1stSearch_TODO.cpp
@@ -0,0 +1,55 @@
+/*
+This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+GS
+Mid-2022
+
+It's about time I learn and practice, once and for all, how to:
+1. Write my own binary tree from scratch.
+2. Traverse it via breadth-first search.
+3. Traverse it via depth-first search.
+4. Compare and contrast the two approaches and how their efficiency and speeds/techniques/etc.
+   differ.
+
+STATUS: todo
+
+To compile and run (assuming you've already `cd`ed into this dir):
+```bash
+# NB: you may need to use `-std=gnu++17` instead of `-std=c++17` in order to obtain extra GNU
+# gcc features, including gcc extensions, POSIX cmds, and Linux sytem cmds.
+# See: [my answer]: https://stackoverflow.com/a/71801111/4561887
+
+# 1. In C++
+time g++ -Wall -Wextra -Werror -O3 -std=c++17 hello_world_extra_basic.cpp -o bin/a && bin/a
+```
+
+References:
+1.
+
+*/
+
+// C++ includes
+#include <iostream>  // For `std::cin`, `std::cout`, `std::endl`, etc.
+
+// C includes
+#include <cstdint>  // For `uint8_t`, `int8_t`, etc.
+#include <cstdio>   // For `printf()`
+
+// int main(int argc, char *argv[])  // alternative prototype
+int main()
+{
+    printf("Hello ");
+    std::cout << "world!\n\n";
+
+    return 0;
+}
+
+
+
+/*
+SAMPLE OUTPUT:
+
+    eRCaGuy_hello_world/cpp$ g++ -Wall -Wextra -Werror -O3 -std=c++17 hello_world_extra_basic.cpp -o bin/a && bin/a
+    Hello world!
+
+*/
