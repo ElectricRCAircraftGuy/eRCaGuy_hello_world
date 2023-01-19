13c535620f41457f4317858bd2bde9e447a64315
Gabriel Staples <ercaguy@gmail.com>
2023-01-18 19:37:04 -0700

    Add and fix up a few TODO files in C
    	new file:   c/socket__udp_2way_streaming__command_and_control_TODO.c
    	renamed:    c/socket__udp_server_client.c -> c/socket__udp_server_client_TODO.c
    	renamed:    c/socket__udp_server_to_server.c -> c/socket__udp_server_to_server_TODO.c

---
+++ c/socket__udp_2way_streaming__command_and_control_TODO.c
@@ -0,0 +1,11 @@
+/*
+
+GS
+2022 - Jan. 2023
+
+Make this a "command and control" demo. One side continually streams control commands (think: as
+though from a 6 CH RC Tx), and the other side continually streams back telemetry data (as though
+from an RC vehicle, back to the Tx, for display purposes, like voltage, speed, altitude, heading,
+Euler angles, etc.).
+
+*/
