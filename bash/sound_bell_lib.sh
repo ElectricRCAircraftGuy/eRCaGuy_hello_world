1f8aae33b0c65912e5c8f8d06a264401db29cfdd
Gabriel Staples <ercaguy@gmail.com>
2023-02-01 23:04:04 -0700

    bash/sound_bell_lib.sh: fix bugs in the logic which checks if the...
    ...script is being sourced vs executed.
    
    Note that the old logic would NOT work correctly in the event of
    nesting (one script sourcing or calling another), whereas this new
    logic *does* work, even when scripts are nested!
    
    So: use `"${BASH_SOURCE[0]}" = "$0"`, NOT
    `"${FUNCNAME[-1]}"`!

--- bash/sound_bell_lib.sh
+++ bash/sound_bell_lib.sh
@@ -15,9 +15,13 @@
 # Check this script with: `shellcheck sound_bell_lib.sh`
 
 # Run command:
-#       ./sound_bell_lib.sh
-# Source (import) command to get access to these functions:
-#       . ./sound_bell_lib.sh
+#
+#       bash/sound_bell_lib.sh
+#
+# Source (import) command to get access to any functions herein:
+# [my answer] https://stackoverflow.com/a/62626515/4561887):
+#
+#       . bash/sound_bell_lib.sh
 
 # References:
 # 1. Differences between source and export: [my answer]:
@@ -32,7 +36,8 @@ RETURN_CODE_ERROR=1
 
 # ------------------------------ Library START ---------------------------------
 
-# Play the bell sound "ding" to alert the user of something
+# Play the bell sound "ding" to alert the user of something.
+#
 # Usage:
 #       sound_bell [number_of_times [second_delay_between_sounds]]
 # Examples
@@ -44,7 +49,7 @@ RETURN_CODE_ERROR=1
 sound_bell() {
     # set default values
     num_times="1"
-    delay_sec="0.1"
+    delay_sec="0.12"
 
     # set to user-defined values if the user passes them in as args
     if [ -n "$1" ]; then
@@ -74,10 +79,7 @@ parse_args() {
     return "$RETURN_CODE_SUCCESS"
 }
 
-# run some basic tests if this program is run rather than sourced!
-main() {
-    # unit tests
-
+run_unit_tests() {
     echo "sound_bell";      sleep 1
     sound_bell
 
@@ -94,28 +96,30 @@ main() {
     sound_bell 1
 }
 
-# Set the global variable `run` to "true" if the script is being **executed** (not sourced) and
-# `main` should run, and set `run` to "false" otherwise. One might source this script but intend
-# NOT to run it if they wanted to import functions from the script.
-# See:
-# 1. my answer: https://stackoverflow.com/a/70662049/4561887
-# 1. https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world/blob/master/bash/check_if_sourced_or_executed.sh
-run_check() {
-    # This is akin to `if __name__ == "__main__":` in Python.
-    if [ "${FUNCNAME[-1]}" == "main" ]; then
-        # This script is being EXECUTED, not sourced
-        run="true"
-    fi
+main() {
+    run_unit_tests
 }
+
+# Check if this script is being sourced or executed.
+# See:
+# 1. This answer here where I first learned about `"${BASH_SOURCE[0]}" = "$0"`:
+#    https://stackoverflow.com/a/29967433/4561887
+# 1. My answer here: https://stackoverflow.com/a/70662116/4561887
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+    # This script is being run.
+    __name__="__main__"
+else
+    # This script is being sourced.
+    __name__="__source__"
+fi
 
 # ------------------------------------------------------------------------------
 # Main program entry point
 # ------------------------------------------------------------------------------
 
-# Only run main function if this file is being executed, NOT sourced.
-run="false"
-run_check
-if [ "$run" == "true" ]; then
+# Only run `main` if this script is being **run**, NOT sourced (imported)
+# - See my answer: https://stackoverflow.com/a/70662116/4561887
+if [ "$__name__" = "__main__" ]; then
     parse_args "$@"
     time main
     exit $RETURN_CODE_SUCCESS
@@ -130,4 +134,7 @@ fi
 #       sound_bell 2 .2
 #       sound_bell 3 .5
 #       sound_bell 1
-
+#
+#       real    0m6.340s
+#       user    0m0.011s
+#       sys 0m0.009s
