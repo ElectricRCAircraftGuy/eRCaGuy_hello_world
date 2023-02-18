7f2affd6327a82eae94614df285aa4072e914efe
Gabriel Staples <ercaguy@gmail.com>
2023-02-18 12:44:52 -0700

    Finish bash/ansi_text_format_lib_unittest.sh

--- bash/ansi_text_format_lib_unittest.sh
+++ bash/ansi_text_format_lib_unittest.sh
@@ -3,27 +3,27 @@
 # This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
 
 # GS
-# 1 Feb. 2023
+# Feb. 18 2023
+# https://gabrielstaples.com/
+# https://www.electricrcaircraftguy.com/
 
 # Source (import) the corresponding bash library, and call some functions it
 # contains.
 #
-# Status: wip
-
-# keywords: bell sound; play sounds in bash
+# Status: done and works
 
 # Check this script with:
 #
-##      shellcheck -x bash/sound_bell_lib_unittest.sh
+##      shellcheck -x bash/ansi_text_format_lib_unittest.sh
 #
 # Run command:
 #
-#       bash/sound_bell_lib_unittest.sh
+#       bash/ansi_text_format_lib_unittest.sh
 #
 # Source (import) command to get access to any functions herein:
 # [my answer] https://stackoverflow.com/a/62626515/4561887):
 #
-#       . bash/sound_bell_lib_unittest.sh
+#       . bash/ansi_text_format_lib_unittest.sh
 
 # References:
 # 1.
@@ -37,36 +37,25 @@ SCRIPT_FILENAME="$(basename "$FULL_PATH_TO_SCRIPT")"
 # Source (import) the bash library to test
 # See also my answer here: https://stackoverflow.com/a/75341911/4561887
 # shellcheck source=bash/sound_bell_lib.sh
-. "$SCRIPT_DIRECTORY/sound_bell_lib.sh"
+. "$SCRIPT_DIRECTORY/ansi_text_format_lib.sh"
 
 
 # Now test some of its functions
 
-echo "sound_bell 3 .3";      sleep 1
-sound_bell 3 .3
-
-echo "sound_bell 7 .2";      sleep 1
-sound_bell 7 .2
-
-
-echo "run_unit_tests"
-run_unit_tests
-
-echo "Done!"
+# Set your format string
+set_f ANSI_BOLD ANSI_UNDERLINE ANSI_SLOW_BLINK ANSI_FG_BR_BLU
+# view the current format string
+echo "$f"
+# Now print something in that format
+ansi_echo "This library works very well! I am very pleased with its usage and format! :)"
+f_echo "This library works very well! I am very pleased with its usage and format! :)"
 
 
-
-# SAMPLE OUTPUT:
+# SAMPLE OUTPUT (run it yourself to see the blinking formatted output):
 #
-#       eRCaGuy_hello_world$ bash/sound_bell_lib_unittest.sh
-#       sound_bell 3 .3
-#       sound_bell 7 .2
-#       run_unit_tests
-#       sound_bell
-#       sound_bell 2
-#       sound_bell 2 .2
-#       sound_bell 3 .5
-#       sound_bell 1
-#       Done!
+#       eRCaGuy_hello_world/bash$ ./ansi_text_format_lib_unittest.sh
+#       \e[;1;4;5;94m
+#       This library works very well! I am very pleased with its usage and format! :)
+#       This library works very well! I am very pleased with its usage and format! :)
 
 
