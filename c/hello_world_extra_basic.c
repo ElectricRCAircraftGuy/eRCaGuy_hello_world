3610022e7c3a06e295995bb2f5acc5d56bf83976
Gabriel Staples <ercaguy@gmail.com>
2023-02-18 00:34:25 -0700

    c/hello_world_extra_basic.c: add bash shebang-type 1-liner to top
    ...to make this C file directly executable as a script!

--- c/hello_world_extra_basic.c
+++ c/hello_world_extra_basic.c
@@ -1,3 +1,5 @@
+///usr/bin/env ccache gcc -Wall -Wextra -Werror -O3 -std=gnu17 "$0" -o /tmp/a -lm && /tmp/a "$@"; exit
+
 /*
 This file is part of eRCaGuy_hello_world: https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
 
@@ -9,7 +11,12 @@ To compile and run (assuming you've already `cd`ed into this dir):
 # Why `-std=gnu17` vs `-std=c17`, and `-std=gnu++17` vs `-std=c++17`?
 # See: [my answer]: https://stackoverflow.com/a/71801111/4561887
 
+# [run once ever] install ccache
+sudo apt update && sudo apt install ccache
+
 # 1. In C:
+./hello_world_extra_basic.c
+# or:
 gcc -Wall -Wextra -Werror -O3 -std=gnu17 hello_world_extra_basic.c -o bin/a -lm && bin/a
 
 # 2. In C++
@@ -17,9 +24,15 @@ g++ -Wall -Wextra -Werror -O3 -std=gnu++17 hello_world_extra_basic.c -o bin/a &&
 ```
 
 References:
+1. For help with this funky bash script 1-liner at the very top, to make this C
+   program directly executable:
+    1. Shebang starting with `//`? - https://unix.stackexchange.com/a/162535/114401
+    1. *****+ Run C or C++ file as a script - [my answer] https://stackoverflow.com/a/75491834/4561887
+    1. ***** What's the appropriate Go shebang line? - [my answer] https://stackoverflow.com/a/75491727/4561887
 1.
 
 */
+
 
 #include <stdbool.h> // For `true` (`1`) and `false` (`0`) macros in C
 #include <stdint.h>  // For `uint8_t`, `int8_t`, etc.
@@ -32,6 +45,7 @@ int main()
 
     return 0;
 }
+
 
 /*
 SAMPLE OUTPUT:
@@ -46,6 +60,5 @@ OR, in C++:
 
     eRCaGuy_hello_world/c$ g++ -Wall -Wextra -Werror -O3 -std=c++17 hello_world_extra_basic.c -o bin/a && bin/a
     Hello World.
-
 
 */
