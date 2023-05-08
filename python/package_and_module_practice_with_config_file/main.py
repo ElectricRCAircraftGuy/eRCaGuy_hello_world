a9845baff379626203304e71c3935090d00181df
Gabriel Staples <ercaguy@gmail.com>
2023-05-07 22:27:01 -0700

    python/package_and_module_practice_with_config_file: update readme and main.py

--- python/package_and_module_practice_with_config_file/main.py
+++ python/package_and_module_practice_with_config_file/main.py
@@ -12,10 +12,24 @@ Modified from @Aya's excellent answer here: https://stackoverflow.com/a/16985066
 
 from my_package.add_strings_as_int import add
 
+# works!
+import config
+config._test()
+print("config.MY_CONSTANT2 = {}".format(config.MY_CONSTANT2))
+print()
+
+# also works!
+from config import MY_CONSTANT1
+print("MY_CONSTANT1 = {}".format(MY_CONSTANT1))
+# print("MY_CONSTANT2 = {}".format(MY_CONSTANT2))  # not defined
+print()
+
+
 def main():
     sum = add("1", "1")
-    print(sum)
+    print("sum = {}".format(sum))
     assert sum == 2
+
 
 if __name__ == '__main__':
     main()
