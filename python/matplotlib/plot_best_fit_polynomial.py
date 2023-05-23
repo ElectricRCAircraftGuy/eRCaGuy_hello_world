1702463e7a411a8446d8d269ba1b358f599ecdb6
Gabriel Staples <ercaguy@gmail.com>
2023-05-22 17:03:38 -0700

    Finish python/matplotlib/plot_best_fit_polynomial.py!

--- python/matplotlib/plot_best_fit_polynomial.py
+++ python/matplotlib/plot_best_fit_polynomial.py
@@ -13,7 +13,7 @@ We will plot with `matplotlib`.
 This code was modified from the code from the bottom of this page:
 https://numpy.org/doc/stable/reference/routines.polynomials.classes.html
 
-Status: wip
+Status: done and works!
 
 keywords: (keywords)
 
@@ -46,6 +46,7 @@ References:
 """
 
 # ORIGINAL CODE:
+# From the bottom of here: https://numpy.org/doc/stable/reference/routines.polynomials.classes.html
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -65,19 +66,54 @@ plt.show()
 
 # MY VERSION:
 
-import numpy as np
 import matplotlib.pyplot as plt
-from numpy.polynomial import Chebyshev as T
+import numpy as np
+from numpy.polynomial import Chebyshev
+from numpy.polynomial import Polynomial
 
+# https://numpy.org/doc/stable/reference/random/generated/numpy.random.seed.html
 np.random.seed(11)
-x = np.linspace(0, 2*np.pi, 20)
+
+# Line-space: get evenly-spaced points to plot a line. See:
+# https://numpy.org/doc/stable/reference/generated/numpy.linspace.html
+# Returns a numpy ndarray: https://numpy.org/doc/stable/reference/generated/numpy.ndarray.html
+x = np.linspace(0, 2*np.pi, num=20)
+
+# Create some points from numpy ndarray `x` which are sinusoidal + some normal (Gaussian) noise.
+# See `numpy.random.normal()` here:
+# https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
 y = np.sin(x) + np.random.normal(scale=.1, size=x.shape)
-p = T.fit(x, y, 5)
+
+# For Chebyshev Series, see:
+# 1. https://numpy.org/doc/stable/reference/routines.polynomials.chebyshev.html
+# 1. Class definition:
+#    https://numpy.org/doc/stable/reference/generated/numpy.polynomial.chebyshev.Chebyshev.html
+#
+# Obtain a 5th degree (order) least-squares fit curve to the x, y data
+cheby = Chebyshev.fit(x, y, deg=5)
+# Lines-space: get evenly-spaced points to plot a line; see:
+# https://numpy.org/doc/stable/reference/generated/numpy.polynomial.chebyshev.Chebyshev.linspace.html
+xx_cheby, yy_cheby = cheby.linspace()
+
+print(f"cheby.domain = \n{cheby.domain}")
+print(f"cheby.window = \n{cheby.window}")
+
+# let's do it for a Polynomial series too; see:
+# https://numpy.org/doc/stable/reference/generated/numpy.polynomial.polynomial.Polynomial.html
+poly = Polynomial.fit(x, y, deg=5)
+xx_poly, yy_poly = poly.linspace()
+
+# plot
+
+f1 = plt.figure()
+
 plt.plot(x, y, 'o')
-xx, yy = p.linspace()
-plt.plot(xx, yy, lw=2)
-p.domain
-p.window
+plt.plot(xx_cheby, yy_cheby, linewidth=4,
+   label="Chebyshev Series 5th degree\nleast squares best fit curve")
+plt.plot(xx_poly, yy_poly, 'k', linewidth=1,
+   label="Polynomial Series 5th degree\nleast squares best fit curve")
+
+plt.legend()
 plt.show()
 
 
@@ -85,6 +121,8 @@ plt.show()
 """
 SAMPLE OUTPUT:
 
-A single figure is displayed in its own GUI window.
+A single figure is displayed in its own GUI window. It consists of some dosts scattered around the
+shape of a sine wave, with a thick Chebyshev best fit curve and a thin black Polynomial best fit
+curve on top of that, both in the shape of a sine wave.
 
 """
