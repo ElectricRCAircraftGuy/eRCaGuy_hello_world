81b6b54d150a10a673e64da7ace0cd80e5a4fad5
Gabriel Staples <ercaguy@gmail.com>
2023-10-01 18:28:14 -0700

    python/pandas_dataframe_interpolation_via_scipy_interpolate.py: Add the broken Method 4...
    ...which uses `np.interp()` and cannot extrapolate beyond the input
       x-value bounds.

--- python/pandas_dataframe_interpolation_via_scipy_interpolate.py
+++ python/pandas_dataframe_interpolation_via_scipy_interpolate.py
@@ -32,6 +32,9 @@ python3 pandas_dataframe_interpolation_via_scipy_interpolate.py
 
 References:
 1. GitHub Copilot
+1. https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.interp1d.html#scipy.interpolate.interp1d
+1. https://numpy.org/doc/stable/reference/generated/numpy.arange.html
+1. https://numpy.org/doc/stable/reference/generated/numpy.interp.html
 1.
 
 
@@ -76,6 +79,50 @@ In this example, the `kind` argument is set to `'linear'` and the `fill_value` a
 `'extrapolate'`. This allows the interpolation function to extrapolate beyond the range of `x`
 values in `df2`.
 
+== NEW CONVERSATION: how I obtained Method 4 below: ==
+
+Me:
+> Write an interpolation example using `np.interp()`.
+
+GitHub Copilot: Sure! Here's an example of how to use `np.interp()` to interpolate a set of `x` and
+`y` values to a new set of `x` values:
+
+```python
+import numpy as np
+
+# create some example data
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 4, 6, 8, 10])
+
+# create a new set of x values to interpolate to
+x_new = np.array([1.5, 2.5, 3.5])
+
+# interpolate the y values to the new x values
+y_new = np.interp(x_new, x, y)
+
+# print the interpolated y values
+print(y_new)
+```
+
+In this example, `np.interp()` is used to interpolate the `y` values of the `x` and `y` arrays to a
+new set of `x` values specified by the `x_new` array. The resulting interpolated `y` values are
+stored in the `y_new` array. The output of this example code should be:
+
+```
+[3. 5. 7.]
+```
+
+This means that the interpolated `y` values for the `x` values `[1.5, 2.5, 3.5]` are `[3, 5, 7]`,
+respectively.
+
+Me:
+> np.interp() produces erroneous results when extrapolating beyond the input x values.
+
+GitHub Copilot: You're correct. np.interp() can produce erroneous results when extrapolating beyond
+the input x values...
+[It then told me to use SciPy's `interp1d()` instead, which I already was using
+in Method 3, per its previous advice above.]
+
 
 """
 
@@ -87,7 +134,7 @@ from scipy.interpolate import interp1d
 # =================================
 # [GOOD] Super simple, more-manual way to interpolate
 # - mostly written by GitHub Copilot
-print("==== METHOD 1: manual, using the x-values of the longer dataframe ====\n")
+print("==== [GOOD] METHOD 1: manual, using the x-values of the longer dataframe ====\n")
 # =================================
 
 # assume df1 is the longer dataframe and df2 is the shorter dataframe
@@ -111,7 +158,7 @@ print(f"df_diff:\n{df_diff}\n")
 # =================================
 # [BETTER] Alternative, generic way to do this at the x-values of the longer dataframe
 # - By me
-print("==== METHOD 2: generic, function-based, using the x-values of the longer dataframe ====\n")
+print("==== [BETTER] METHOD 2: generic, function-based, using the x-values of the longer dataframe ====\n")
 # =================================
 
 def interpolate_df(x1, y1, x2, y2):
@@ -166,9 +213,10 @@ print(f"df2:\n{df2}\n")
 print(f"df_new_interpolated:\n{df_new_interpolated}\n")
 
 # =================================
-# [BEST, perhaps] Alternative, generic way to do this at fixed x-value intervals
+# [BEST!!!] Alternative, generic way to do this at fixed x-value intervals
 # - By me, with CoPilot's assistance
-print("==== METHOD 3: generic, function-based, at fixed x-value intervals ====\n")
+print("==== [BEST] METHOD 3: generic, function-based, at fixed x-value intervals; uses\n"
+      "`scipy.interpolate.interp1d()` ====\n")
 # =================================
 
 def interpolate_df_at_fixed_x_intervals(x1, y1, x2, y2, x_interval):
@@ -220,6 +268,43 @@ print(f"df1:\n{df1}\n")
 print(f"df2:\n{df2}\n")
 print(f"df_new_interpolated_at_fixed_x_intervals:\n{df_new_interpolated_at_fixed_x_intervals}\n")
 
+# =================================
+# [less-robust (and erroneous: CANNOT EXTRAPOLATE) alternative to using SciPy's `interp1d()`]
+print("==== [DO NOT USE! PRODUCES ERRONEOUS RESULTS WHEN EXTRAPOLATING OUTSIDE OF THE INPUT\n"
+      "X VALUES; all y_diff values should be zeros, but are not here!] METHOD 4: using\n"
+      "`np.interp()` at fixed x-value intervals ====")
+# =================================
+
+def interpolate_df_at_fixed_x_intervals_numpy(x1, y1, x2, y2, x_interval):
+    """
+    Interpolate dataframes at fixed x intervals, but this time using `np.interp()` instead of
+    `scipy.interpolate.interp1d()`.
+    """
+    x_min = min(x1.min(), x2.min())
+    x_max = max(x1.max(), x2.max())
+
+    x_new = np.arange(x_min, x_max + x_interval, x_interval)
+    y1_new = np.interp(x_new, x1, y1)
+    y2_new = np.interp(x_new, x2, y2)
+    y_diff = y1_new - y2_new
+
+    df_interpolated = pd.DataFrame({
+        x1.name + "_new": x_new,     # the new set of x-values; arbitrarily use `x1`'s name
+        y1.name + "_new": y1_new,    # the new, interpolated y1 values
+        y2.name + "_new": y2_new,    # the new, interpolated y2 values
+        "y_diff": y_diff,
+    })
+
+    return df_interpolated  # Contains x_new, y1_new, y2_new, and y_diff
+
+df_new_interpolated_at_fixed_x_intervals_numpy = interpolate_df_at_fixed_x_intervals_numpy(
+    df1['x1'], df1['y1'], df2['x2'], df2['y2'], x_interval=0.25)
+
+print(f"df1:\n{df1}\n")
+print(f"df2:\n{df2}\n")
+print(f"df_new_interpolated_at_fixed_x_intervals_numpy:\n"
+      f"{df_new_interpolated_at_fixed_x_intervals_numpy}\n")
+
 
 
 # pylint: disable-next=pointless-string-statement
@@ -229,7 +314,7 @@ SAMPLE OUTPUT:
 eRCaGuy_hello_world$ python/pandas_dataframe_interpolation_via_scipy_interpolate.py
 /usr/lib/python3/dist-packages/scipy/__init__.py:146: UserWarning: A NumPy version >=1.17.3 and <1.25.0 is required for this version of SciPy (detected version 1.25.2
   warnings.warn(f"A NumPy version >={np_minversion} and <{np_maxversion}"
-==== METHOD 1: manual, using the x-values of the longer dataframe ====
+==== [GOOD] METHOD 1: manual, using the x-values of the longer dataframe ====
 
 df1:
    x   y
@@ -261,7 +346,7 @@ df_diff:
 3  4  0.0
 4  5  0.0
 
-==== METHOD 2: generic, function-based, using the x-values of the longer dataframe ====
+==== [BETTER] METHOD 2: generic, function-based, using the x-values of the longer dataframe ====
 
 df1:
    x1  y1
@@ -285,7 +370,8 @@ df_new_interpolated:
 3   4   8              8.0     0.0
 4   5  10             10.0     0.0
 
-==== METHOD 3: generic, function-based, at fixed x-value intervals ====
+==== [BEST] METHOD 3: generic, function-based, at fixed x-value intervals; uses
+`scipy.interpolate.interp1d()` ====
 
 df1:
    x1  y1
@@ -321,5 +407,41 @@ df_new_interpolated_at_fixed_x_intervals:
 15    4.75     9.5     9.5     0.0
 16    5.00    10.0    10.0     0.0
 
+==== [DO NOT USE! PRODUCES ERRONEOUS RESULTS WHEN EXTRAPOLATING OUTSIDE OF THE INPUT
+X VALUES; all y_diff values should be zeros, but are not here!] METHOD 4: using
+`np.interp()` at fixed x-value intervals ====
+df1:
+   x1  y1
+0   1   2
+1   2   4
+2   3   6
+3   4   8
+4   5  10
+
+df2:
+    x2  y2
+0  1.5   3
+1  2.5   5
+2  3.5   7
+
+df_new_interpolated_at_fixed_x_intervals_numpy:
+    x1_new  y1_new  y2_new  y_diff
+0     1.00     2.0     3.0    -1.0
+1     1.25     2.5     3.0    -0.5
+2     1.50     3.0     3.0     0.0
+3     1.75     3.5     3.5     0.0
+4     2.00     4.0     4.0     0.0
+5     2.25     4.5     4.5     0.0
+6     2.50     5.0     5.0     0.0
+7     2.75     5.5     5.5     0.0
+8     3.00     6.0     6.0     0.0
+9     3.25     6.5     6.5     0.0
+10    3.50     7.0     7.0     0.0
+11    3.75     7.5     7.0     0.5
+12    4.00     8.0     7.0     1.0
+13    4.25     8.5     7.0     1.5
+14    4.50     9.0     7.0     2.0
+15    4.75     9.5     7.0     2.5
+16    5.00    10.0     7.0     3.0
 
 """
