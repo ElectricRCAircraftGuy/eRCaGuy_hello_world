f4397f39ed6720dd988b189351b1941001b8eba2
Gabriel Staples <ercaguy@gmail.com>
2023-11-21 22:04:26 -0700

    c/static_extern_function_include__module_wrapper.h: add comments about `extern`

--- c/static_extern_function_include__module_wrapper.h
+++ c/static_extern_function_include__module_wrapper.h
@@ -12,3 +12,11 @@ This file is "included" (used) by `static_extern_function_include__main.c`. Run 
 void print_incrementing_number_wrapper();
 void set_counter(uint32_t new_counter);
 uint32_t get_counter();
+
+// Also fine, but for **functions**, `extern` is automatically implicitly applied and does NOT have
+// to be explicitly written! 
+// - See my answer here: https://stackoverflow.com/a/77527374/4561887
+// 
+// extern void print_incrementing_number_wrapper();
+// extern void set_counter(uint32_t new_counter);
+// extern uint32_t get_counter();
