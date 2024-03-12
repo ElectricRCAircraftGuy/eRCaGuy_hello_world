9f4de966b42b1a366078ef5b592cdc0f5eba1336
Gabriel Staples <ercaguy@gmail.com>
2024-03-11 23:20:42 -0700

    c/sizeof_compile_time_lib_test_BEST.c: add more compile-time...
    ...sizeof checks on bool, int, and double types, as a demo.

--- c/sizeof_compile_time_lib_test_BEST.c
+++ c/sizeof_compile_time_lib_test_BEST.c
@@ -72,6 +72,10 @@ struct My_struct2_s
 COMPILE_TIME_PRINT_SIZEOF_GLOBAL(My_struct);                // 24
 COMPILE_TIME_PRINT_SIZEOF_GLOBAL(struct My_struct2_s);      // 8
 
+COMPILE_TIME_PRINT_SIZEOF_GLOBAL(bool);                     // 1
+COMPILE_TIME_PRINT_SIZEOF_GLOBAL(int);                      // 4
+COMPILE_TIME_PRINT_SIZEOF_GLOBAL(double);                   // 8
+
 int main()
 {
     printf("Testing 'sizeof_compile_time_lib.h'.\n\n");
@@ -80,6 +84,10 @@ int main()
     COMPILE_TIME_PRINT_SIZEOF_LOCAL(My_struct);             // 24
     COMPILE_TIME_PRINT_SIZEOF_LOCAL(struct My_struct2_s);   // 8
     COMPILE_TIME_PRINT_SIZEOF_LOCAL(my_structs);            // 240
+
+    COMPILE_TIME_PRINT_SIZEOF_LOCAL(bool);                  // 1
+    COMPILE_TIME_PRINT_SIZEOF_LOCAL(int);                   // 4
+    COMPILE_TIME_PRINT_SIZEOF_LOCAL(double);                // 8
 
     return 0;
 }
@@ -93,45 +101,95 @@ In C:
 eRCaGuy_hello_world/c$ gcc -Wall -Wextra -Werror -O3 -std=gnu17 sizeof_compile_time_lib_test_BEST.c -o bin/a -lm && bin/a
 In file included from sizeof_compile_time_lib_test_BEST.c:48:
 sizeof_compile_time_lib_test_BEST.c: In function ‘compile_time_sizeof__line_72’:
-sizeof_compile_time_lib.h:68:13: error: case value ‘24’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_72’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘24’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_72’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib.h:82:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   82 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
       |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c:72:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
-   72 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(My_struct);
+   72 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(My_struct);                // 24
       | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c: In function ‘compile_time_sizeof__line_73’:
-sizeof_compile_time_lib.h:68:13: error: case value ‘8’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_73’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_73’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib.h:82:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   82 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
       |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c:73:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
-   73 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(struct My_struct2_s);
+   73 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(struct My_struct2_s);      // 8
+      | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c: In function ‘compile_time_sizeof__line_75’:
+sizeof_compile_time_lib.h:72:13: error: case value ‘1’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_75’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c:75:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
+   75 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(bool);                     // 1
+      | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c: In function ‘compile_time_sizeof__line_76’:
+sizeof_compile_time_lib.h:72:13: error: case value ‘4’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_76’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c:76:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
+   76 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(int);                      // 4
+      | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c: In function ‘compile_time_sizeof__line_77’:
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_77’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c:77:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
+   77 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(double);                   // 8
       | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c: In function ‘main’:
-sizeof_compile_time_lib.h:68:13: error: case value ‘24’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_80’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘24’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_84’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib_test_BEST.c:80:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   80 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(My_struct);
+sizeof_compile_time_lib_test_BEST.c:84:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   84 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(My_struct);             // 24
       |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sizeof_compile_time_lib.h:68:13: error: case value ‘8’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_81’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_85’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib_test_BEST.c:81:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   81 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(struct My_struct2_s);
+sizeof_compile_time_lib_test_BEST.c:85:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   85 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(struct My_struct2_s);   // 8
       |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sizeof_compile_time_lib.h:68:13: error: case value ‘240’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_82’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘240’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_86’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib_test_BEST.c:82:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   82 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(my_structs);
+sizeof_compile_time_lib_test_BEST.c:86:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(my_structs);            // 240
+      |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib.h:72:13: error: case value ‘1’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_88’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib_test_BEST.c:88:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   88 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(bool);                  // 1
+      |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib.h:72:13: error: case value ‘4’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_89’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib_test_BEST.c:89:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   89 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(int);                   // 4
+      |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘enum This_is_the_size_of_your_type_on__line_90’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib_test_BEST.c:90:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   90 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(double);                // 8
       |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 cc1: all warnings being treated as errors
+
+
 
 
 
@@ -140,45 +198,94 @@ OR, in C++:
 eRCaGuy_hello_world/c$ g++ -Wall -Wextra -Werror -O3 -std=gnu++17 sizeof_compile_time_lib_test_BEST.c -o bin/a && bin/a
 In file included from sizeof_compile_time_lib_test_BEST.c:48:
 sizeof_compile_time_lib_test_BEST.c: In function ‘void compile_time_sizeof__line_72()’:
-sizeof_compile_time_lib.h:68:13: error: case value ‘24’ not in enumerated type ‘compile_time_sizeof__line_72()::This_is_the_size_of_your_type_on__line_72’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘24’ not in enumerated type ‘compile_time_sizeof__line_72()::This_is_the_size_of_your_type_on__line_72’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib.h:82:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   82 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
       |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c:72:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
-   72 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(My_struct);
+   72 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(My_struct);                // 24
       | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c: In function ‘void compile_time_sizeof__line_73()’:
-sizeof_compile_time_lib.h:68:13: error: case value ‘8’ not in enumerated type ‘compile_time_sizeof__line_73()::This_is_the_size_of_your_type_on__line_73’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘compile_time_sizeof__line_73()::This_is_the_size_of_your_type_on__line_73’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib.h:82:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   82 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
       |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c:73:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
-   73 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(struct My_struct2_s);
+   73 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(struct My_struct2_s);      // 8
+      | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c: In function ‘void compile_time_sizeof__line_75()’:
+sizeof_compile_time_lib.h:72:13: error: case value ‘1’ not in enumerated type ‘compile_time_sizeof__line_75()::This_is_the_size_of_your_type_on__line_75’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c:75:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
+   75 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(bool);                     // 1
+      | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c: In function ‘void compile_time_sizeof__line_76()’:
+sizeof_compile_time_lib.h:72:13: error: case value ‘4’ not in enumerated type ‘compile_time_sizeof__line_76()::This_is_the_size_of_your_type_on__line_76’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c:76:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
+   76 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(int);                      // 4
+      | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c: In function ‘void compile_time_sizeof__line_77()’:
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘compile_time_sizeof__line_77()::This_is_the_size_of_your_type_on__line_77’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib.h:86:9: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |         COMPILE_TIME_PRINT_SIZEOF_LOCAL(variable_or_data_type); \
+      |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib_test_BEST.c:77:1: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_GLOBAL’
+   77 | COMPILE_TIME_PRINT_SIZEOF_GLOBAL(double);                   // 8
       | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sizeof_compile_time_lib_test_BEST.c: In function ‘int main()’:
-sizeof_compile_time_lib.h:68:13: error: case value ‘24’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_80’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘24’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_84’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib_test_BEST.c:80:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   80 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(My_struct);
+sizeof_compile_time_lib_test_BEST.c:84:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   84 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(My_struct);             // 24
       |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sizeof_compile_time_lib.h:68:13: error: case value ‘8’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_81’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_85’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib_test_BEST.c:81:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   81 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(struct My_struct2_s);
+sizeof_compile_time_lib_test_BEST.c:85:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   85 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(struct My_struct2_s);   // 8
       |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sizeof_compile_time_lib.h:68:13: error: case value ‘240’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_82’ [-Werror=switch]
-   68 |             case sizeof(variable_or_data_type): \
+sizeof_compile_time_lib.h:72:13: error: case value ‘240’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_86’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
       |             ^~~~
-sizeof_compile_time_lib_test_BEST.c:82:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
-   82 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(my_structs);
+sizeof_compile_time_lib_test_BEST.c:86:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   86 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(my_structs);            // 240
+      |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib.h:72:13: error: case value ‘1’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_88’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib_test_BEST.c:88:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   88 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(bool);                  // 1
+      |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib.h:72:13: error: case value ‘4’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_89’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib_test_BEST.c:89:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   89 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(int);                   // 4
+      |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sizeof_compile_time_lib.h:72:13: error: case value ‘8’ not in enumerated type ‘main()::This_is_the_size_of_your_type_on__line_90’ [-Werror=switch]
+   72 |             case sizeof(variable_or_data_type): \
+      |             ^~~~
+sizeof_compile_time_lib_test_BEST.c:90:5: note: in expansion of macro ‘COMPILE_TIME_PRINT_SIZEOF_LOCAL’
+   90 |     COMPILE_TIME_PRINT_SIZEOF_LOCAL(double);                // 8
       |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 cc1plus: all warnings being treated as errors
+
 
 
 */
