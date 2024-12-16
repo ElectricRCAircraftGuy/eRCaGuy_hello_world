28c1e7ca8f08a93dd42bada53339acd1d7c53b58
Gabriel Staples <ercaguy@gmail.com>
2024-12-16 08:21:12 -0700

    Completely re-write python/time_monotonic_ns__get_precision.py
    ...to check 3 different timing techniques and properly work on Windows
    too.
    
    Use Pandas for high-speed data analysis.

--- python/time_monotonic_ns__get_precision.py
+++ python/time_monotonic_ns__get_precision.py
@@ -31,75 +31,198 @@ python3 time_monotonic_ns__get_precision.py
 References:
 1. *****+See my answer with this code, here: 
    https://stackoverflow.com/a/73482099/4561887
+1. My answer: How to iterate over Pandas DataFrames without iterating: 
+   https://stackoverflow.com/a/77270285/4561887
 
 """
 
-
-import statistics
+import os
+import pandas as pd
 import time
 
-# preallocated list, so that no dynamic memory allocation will happen in 
-# the loop below
-ARRAY_SIZE = 20
-timestamps_ns = [None]*ARRAY_SIZE
+SAMPLE_SIZE_DEFAULT = 20000
+# For cases where Windows may have really crappy 16ms precision, we need a
+# significantly larger sample size.
+SAMPLE_SIZE_MIN_FOR_WINDOWS = 20000000
+DEBUG = False  # Set to True to enable debug prints
+
+def debug_print(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
+
+def print_bar():
+    debug_print("="*56, "\n")
+
+def process_timestamps(timestamps_ns, output_stats_header_str):
+    """
+    Process the timestamps list to determine the time precision of the system.
+    """
+
+    # Create a pandas DataFrame for efficient analysis of large datasets
+    df = pd.DataFrame({"timestamp_ns": timestamps_ns}, dtype='int64')
+    debug_print(f"df original:\n{df}")
+    print_bar()
+
+    # Remove duplicate timestamps. On Linux, there won't be any, because it has
+    # sub-microsecond precision, but on Windows, the dataset may be mostly
+    # duplicates because repeated calls to `time.monotonic_ns()` may return the
+    # same value if called in quick succession.
+    df.drop_duplicates(inplace=True)
+    debug_print(f"df no duplicates:\n{df}")
+    print_bar()
+    if len(df) < 2:
+        print("Error: not enough data to calculate time precision. Try \n"
+              "increasing `SAMPLE_SIZE` by a factor of 10, and try again.")
+        exit(1)
+
+    # Now calculate the time differences between the timestamps.
+    df["previous_timestamp_ns"] = df["timestamp_ns"].shift(1)
+    df = df.dropna()  # remove NaN row
+    df["previous_timestamp_ns"] = df["previous_timestamp_ns"].astype('int64')
+    df["delta_time_us"] = (
+        df["timestamp_ns"] - df["previous_timestamp_ns"]) / 1e3
+    debug_print(f"df:\n{df}")
+    print_bar()
+
+    # Output statistics
+    
+    mean = df["delta_time_us"].mean()
+    median = df["delta_time_us"].median()
+    mode = df["delta_time_us"].mode()[0]
+    stdev = df["delta_time_us"].std()
+
+    print(f">>>>>>>>>> {output_stats_header_str} <<<<<<<<<<")
+    print(f"Mean:   {mean:.3f} us")
+    print(f"Median: {median:.3f} us")
+    print(f"Mode:   {mode:.3f} us")
+    print(f"Stdev:  {stdev:.3f} us")
+    print(f"FINAL ANSWER: time precision on this system: "
+        + f"{median:.3f} +/- {stdev:.3f} us\n")
+
+# =============================================================================
+# 1. Test `time.monotonic_ns()`
+# =============================================================================
+
+SAMPLE_SIZE = SAMPLE_SIZE_DEFAULT
+if os.name == 'nt':
+    # The OS is Windows
+    if SAMPLE_SIZE < SAMPLE_SIZE_MIN_FOR_WINDOWS:
+        SAMPLE_SIZE = SAMPLE_SIZE_MIN_FOR_WINDOWS
+        print(f"Detected: running on Windows. Using a larger SAMPLE_SIZE of "
+            f"{SAMPLE_SIZE}.\n")
 
 # Gather timestamps with zero delays between them
+# - preallocated list, so that no dynamic memory allocation will happen in the
+#   loop below
+timestamps_ns = [None]*SAMPLE_SIZE
 for i in range(len(timestamps_ns)):
     timestamps_ns[i] = time.monotonic_ns()
 
-# Now calculate the time differences between the timestamps.
-delta_times_us = [None]*(len(timestamps_ns) - 1)
-t_start_ns = timestamps_ns[0]
-for i in range(1, len(timestamps_ns)):
-    t_now_ns = timestamps_ns[i]
-    dt_ns = t_now_ns - t_start_ns
-    delta_times_us[i - 1] = dt_ns / 1e3
-    t_start_ns = t_now_ns
-    print(f"{i - 1}: dt_ns = {dt_ns}")
+process_timestamps(timestamps_ns, "1. time.monotonic_ns()")
 
-# Output statistics
-mean = statistics.mean(delta_times_us)
-median = statistics.median(delta_times_us)
-mode = statistics.mode(delta_times_us)
-stdev = statistics.stdev(delta_times_us)
-print(f"Mean:   {mean:.3f} us")
-print(f"Median: {median:.3f} us")
-print(f"Mode:   {mode:.3f} us")
-print(f"Stdev:  {stdev:.3f} us")
-print(f"\nFINAL ANSWER: time precision on this system: "
-    + f"{median:.3f} +/- {stdev:.3f} us\n")
+# =============================================================================
+# 2. Test `time.perf_counter_ns()`
+# =============================================================================
 
+SAMPLE_SIZE = SAMPLE_SIZE_DEFAULT
+
+timestamps_ns = [None]*SAMPLE_SIZE
+for i in range(len(timestamps_ns)):
+    timestamps_ns[i] = time.perf_counter_ns()
+
+process_timestamps(timestamps_ns, "2. time.perf_counter_ns()")
+
+# =============================================================================
+# 3. Test `time.time_ns()`
+# =============================================================================
+
+SAMPLE_SIZE = SAMPLE_SIZE_DEFAULT
+if os.name == 'nt':
+    # The OS is Windows
+    if SAMPLE_SIZE < SAMPLE_SIZE_MIN_FOR_WINDOWS:
+        SAMPLE_SIZE = SAMPLE_SIZE_MIN_FOR_WINDOWS
+        print(f"Detected: running on Windows. Using a larger SAMPLE_SIZE of "
+            f"{SAMPLE_SIZE}.\n")
+
+timestamps_ns = [None]*SAMPLE_SIZE
+for i in range(len(timestamps_ns)):
+    timestamps_ns[i] = time.time_ns()
+
+process_timestamps(timestamps_ns, "3. time.time_ns()")
 
 
 # pylint: disable-next=pointless-string-statement
 """
 SAMPLE OUTPUT:
 
-eRCaGuy_hello_world$ python/time_monotonic_ns__get_precision.py 
-0: dt_ns = 333
-1: dt_ns = 153
-2: dt_ns = 80
-3: dt_ns = 78
-4: dt_ns = 72
-5: dt_ns = 71
-6: dt_ns = 72
-7: dt_ns = 71
-8: dt_ns = 64
-9: dt_ns = 65
-10: dt_ns = 62
-11: dt_ns = 63
-12: dt_ns = 61
-13: dt_ns = 130
-14: dt_ns = 66
-15: dt_ns = 63
-16: dt_ns = 61
-17: dt_ns = 62
-18: dt_ns = 62
-Mean:   0.089 us
-Median: 0.066 us
-Mode:   0.062 us
-Stdev:  0.064 us
+-----------
+ON LINUX:
+-----------
 
-FINAL ANSWER: time precision on this system: 0.066 +/- 0.064 us
+eRCaGuy_hello_world$ time python/time_monotonic_ns__get_precision.py 
+>>>>>>>>>> 1. time.monotonic_ns() <<<<<<<<<<
+Mean:   0.081 us
+Median: 0.070 us
+Mode:   0.070 us
+Stdev:  0.118 us
+FINAL ANSWER: time precision on this system: 0.070 +/- 0.118 us
+
+>>>>>>>>>> 2. time.perf_counter_ns() <<<<<<<<<<
+Mean:   0.076 us
+Median: 0.069 us
+Mode:   0.068 us
+Stdev:  0.070 us
+FINAL ANSWER: time precision on this system: 0.069 +/- 0.070 us
+
+>>>>>>>>>> 3. time.time_ns() <<<<<<<<<<
+Mean:   0.080 us
+Median: 0.074 us
+Mode:   -0.030 us
+Stdev:  0.226 us
+FINAL ANSWER: time precision on this system: 0.074 +/- 0.226 us
+
+
+real	0m0.264s
+user	0m0.802s
+sys	0m1.124s
+
+
+
+
+-----------
+ON WINDOWS:
+-----------
+
+eRCaGuy_hello_world$ time python/time_monotonic_ns__get_precision.py
+Detected: running on Windows. Using a larger SAMPLE_SIZE of 20000000.
+
+>>>>>>>>>> 1. time.monotonic_ns() <<<<<<<<<<
+Mean:   15625.000 us
+Median: 16000.000 us
+Mode:   16000.000 us
+Stdev:  486.897 us
+FINAL ANSWER: time precision on this system: 16000.000 +/- 486.897 us
+
+>>>>>>>>>> 2. time.perf_counter_ns() <<<<<<<<<<
+Mean:   0.101 us
+Median: 0.100 us
+Mode:   0.100 us
+Stdev:  0.021 us
+FINAL ANSWER: time precision on this system: 0.100 +/- 0.021 us
+
+Detected: running on Windows. Using a larger SAMPLE_SIZE of 20000000.
+
+>>>>>>>>>> 3. time.time_ns() <<<<<<<<<<
+Mean:   9639.436 us
+Median: 10134.354 us
+Mode:   610.144 us
+Stdev:  5201.053 us
+FINAL ANSWER: time precision on this system: 10134.354 +/- 5201.053 us
+
+
+real    0m8.301s
+user    0m0.000s
+sys     0m0.000s
+
 
 """
