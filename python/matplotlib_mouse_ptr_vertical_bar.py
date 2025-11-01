4f5a8d8172344c23b93e32fc8e32199ea977e50e
Gabriel Staples <ercaguy@gmail.com>
2025-10-31 17:20:27 -0700

    Add AI-written initial draft of python/matplotlib_mouse_ptr_vertical_bar.py

---
+++ python/matplotlib_mouse_ptr_vertical_bar.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""
+This file is part of eRCaGuy_hello_world:
+https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world
+
+GS
+31 Oct. 2025
+
+Try to get a vertical bar to follow the mouse pointer in a matplotlib plot.
+
+References:
+1. Grok chat: https://grok.com/c/5a3ebeb8-3aed-4f2a-97e6-7b3a6eb3fd82
+    Prompt: 
+    > can matplotlib be made so that a vertical bar is shown live on the plots as I move my mouse
+      around?
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.backend_bases import MouseEvent
+
+
+class Cursor:
+    """
+    A cross hair cursor.
+    """
+    def __init__(self, ax):
+        self.ax = ax
+        self.horizontal_line = ax.axhline(color='k', lw=0.8, ls='--')
+        self.vertical_line = ax.axvline(color='k', lw=0.8, ls='--')
+        # text location in axes coordinates
+        self.text = ax.text(0.72, 0.9, '', transform=ax.transAxes)
+
+    def set_cross_hair_visible(self, visible):
+        need_redraw = self.horizontal_line.get_visible() != visible
+        self.horizontal_line.set_visible(visible)
+        self.vertical_line.set_visible(visible)
+        self.text.set_visible(visible)
+        return need_redraw
+
+    def on_mouse_move(self, event):
+        if not event.inaxes:
+            need_redraw = self.set_cross_hair_visible(False)
+            if need_redraw:
+                self.ax.figure.canvas.draw()
+        else:
+            self.set_cross_hair_visible(True)
+            x, y = event.xdata, event.ydata
+            # update the line positions
+            self.horizontal_line.set_ydata([y])
+            self.vertical_line.set_xdata([x])
+            self.text.set_text(f'x={x:1.2f}, y={y:1.2f}')
+            self.ax.figure.canvas.draw()
+
+
+x = np.arange(0, 1, 0.01)
+y = np.sin(2 * 2 * np.pi * x)
+
+fig, ax = plt.subplots()
+ax.set_title('Simple cursor')
+ax.plot(x, y, 'o')
+cursor = Cursor(ax)
+fig.canvas.mpl_connect('motion_notify_event', cursor.on_mouse_move)
+
+# Simulate a mouse move to (0.5, 0.5), needed for online docs
+t = ax.transData
+MouseEvent(
+    "motion_notify_event", ax.figure.canvas, *t.transform((0.5, 0.5))
+)._process()
+
+plt.show()
