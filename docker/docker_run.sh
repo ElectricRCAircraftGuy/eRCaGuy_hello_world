ca0b998e2979a3e9d8f95d8077a7020413e9219c
Gabriel Staples <ercaguy@gmail.com>
2026-02-23 08:15:46 -0700

    docker_run.sh: improve architecture and add main() func
    This follows my architectural best practices I model here:
    https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world/blob/master/bash/argument_parsing__3_advanced__gen_prog_template.sh

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -17,9 +17,13 @@ SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 . "${SCRIPT_DIRECTORY}/../bash/ansi_color_codes_simple_lib.sh" # `echo_green` et al
 . "${SCRIPT_DIRECTORY}/docker_config.sh"  # For `IMAGE_NAME` and `IMAGE_TAG` variables
 
-REPO_ROOT_DIR="$(git rev-parse --show-toplevel)"
+REPO_ROOT_DIR="$(cd "$SCRIPT_DIRECTORY" && git rev-parse --show-toplevel)"
+
+RETURN_CODE_SUCCESS=0
+RETURN_CODE_ERROR=1
 
 EXECUTABLE_NAME="$(basename "$0")"
+
 print_help() {
     cat <<EOF
 Usage: $EXECUTABLE_NAME [OPTIONS] [-- COMMAND [ARGS...]]
@@ -66,102 +70,139 @@ HOST BIND MOUNTS: HOST-DIR -> CONTAINER-DIR:
 EOF
 }
 
-# Parse our own flags that come BEFORE `--`.
-# Everything after `--` is passed through verbatim to the container command.
-# This follows the POSIX/Unix convention used by git, ssh, find, etc.
-#
-# Usage examples:
-#   ./docker_run.sh                        # interactive bash shell, verbose
-#   ./docker_run.sh --quiet                # interactive bash shell, quiet
-#   ./docker_run.sh -- myprogram --quiet   # passes --quiet to myprogram, not us
-#   ./docker_run.sh -q -- make --quiet     # quiet docker_run.sh; passes --quiet to make
-#
-QUIET="false"
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        # Help menu
-        "--help"|"-h"|"-?")
-            print_help
-            exit 0
-            ;;
-        # Quiet: don't echo nor print log messages
-        "--quiet"|"-q")
-            QUIET="true"
-            shift # past argument
-            ;;
-        # End of our flags; everything remaining is passed through to the container command
-        "--")
-            shift # past argument
-            break
-            ;;
-        # Stop parsing our flags at the first unrecognized arg; pass it and everything after through
-        *)
-            break
-            ;;
-    esac
-done
-# Remaining args in "$@" are now passed through verbatim to the container command
+parse_args() {
+    # Parse our own flags that come BEFORE `--`.
+    # Everything after `--` is passed through verbatim to the container command.
+    # This follows the POSIX/Unix convention used by git, ssh, find, etc.
+    #
+    # Usage examples:
+    #   ./docker_run.sh                        # interactive bash shell, verbose
+    #   ./docker_run.sh --quiet                # interactive bash shell, quiet
+    #   ./docker_run.sh -- myprogram --quiet   # passes --quiet to myprogram, not us
+    #   ./docker_run.sh -q -- make --quiet     # quiet docker_run.sh; passes --quiet to make
+    #
+    QUIET="false"
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            # Help menu
+            "--help"|"-h"|"-?")
+                print_help
+                exit "$RETURN_CODE_SUCCESS"
+                ;;
+            # Quiet: don't echo nor print log messages
+            "--quiet"|"-q")
+                QUIET="true"
+                shift # past argument
+                ;;
+            # End of our flags; everything remaining is passed through to the container command
+            "--")
+                shift # past argument
+                break
+                ;;
+            # Stop parsing our flags at the first unrecognized arg; pass it and everything after through
+            *)
+                break
+                ;;
+        esac
+    done
+    # Remaining args in "$@" are now passed through verbatim to the container command
+} # parse_args
 
 # Print only when not in quiet mode
-# - Return true if if `$QUIET` is not "true", to return a non-error exit code regardless.
-log()      { [ "$QUIET" != "true" ] && echo "$@"      || true; }
-log_blue() { [ "$QUIET" != "true" ] && echo_blue "$@" || true; }
+log() {
+    if [ "$QUIET" != "true" ]; then
+        echo "$@"
+    fi
+}
 
-echo "Running Docker image: ${IMAGE_NAME}:${IMAGE_TAG}"
-echo ""
-log "Running as user UID=$(id -u) and group GID=$(id -g)"
-log_blue "Mounting host dirs to container dirs:"
-log_blue "- ${REPO_ROOT_DIR} -> ${REPO_ROOT_DIR}"
-log ""
+# Print in blue only when not in quiet mode
+log_blue() {
+    if [ "$QUIET" != "true" ]; then
+        echo_blue "$@"
+    fi
+}
 
-# Replicate a container name somewhat similar to what `docker compose run` would do if we were
-# using `docker compose run` instead of `docker run`
-#
-# Last 8 chars of a hash over the "sec.ns" timestamp since epoch.
-UNIQUE_HASH="$(date "+%s.%N" | sha256sum | cut -c1-8)"
-CONTAINER_NAME="${IMAGE_NAME}-${IMAGE_TAG}-${UNIQUE_HASH}"
+main() {
+    echo "Running Docker image: ${IMAGE_NAME}:${IMAGE_TAG}"
+    log ""
+    log "Running as user UID=$(id -u) and group GID=$(id -g)"
+    log_blue "Mounting host dirs to container dirs:"
+    log_blue "- ${REPO_ROOT_DIR} -> ${REPO_ROOT_DIR}"
+    log ""
 
-# Run a container instance of the Docker image and tag "${IMAGE_NAME}:${IMAGE_TAG}" with the
-# following options:
-#
-# * --rm = Automatically remove the container when it exits.
-# * -it = Give the container an interactive terminal.
-# * --env USER_ID=$(id -u) = Pass the host user's UID to the container (for gosu).
-# * --env GROUP_ID=$(id -g) = Pass the host user's GID to the container (for gosu).
-# * --env ... = other environmental variable options.
-# * --volume /HOST-DIR:/CONTAINER-DIR = Bind mount the workspace directory outside the docker
-#     container to inside the container so you can access your code.
-# *   Also mount the host's `~/.bashrc` file to the container as read-only, to be used by the
-#     container's ENTRYPOINT script if desired.
-# * --workdir = Set the working directory inside the container. This is where the interactive
-#     prompt will start.
-# * --name = set this as the container name as shown by `docker ps` outside the container but
-#     while it is running.
-# * --hostname = set the container's hostname, as shown by `hostname` and in the PS1
-#     prompt string as `user@hostname` inside the running container.
-# * "${IMAGE_NAME}:${IMAGE_TAG}" = The name of the already-built Docker image and tag to run.
-# * "$@" = All additional arguments passed to this script, forwarded to `docker run`.
-#      Ex: `./docker_run.sh bash -c "cd cpp && x86_64-w64-mingw32-g++ --version"`
-#
-docker_args=(
-    --rm
-    -it
-    # Pass user/group IDs and names for proper file permissions (used by gosu in ENTRYPOINT)
-    --env QUIET="${QUIET}"
-    --env USER_ID="$(id -u)"
-    --env GROUP_ID="$(id -g)"
-    --env USER_NAME="$(id -un)"
-    --env GROUP_NAME="$(id -gn)"
-    # Directory to take ownership of inside the container
-    --env TAKE_OWNERSHIP_OF_DIR1="${REPO_ROOT_DIR}"
-    --env REPO_ROOT_DIR="${REPO_ROOT_DIR}"
-    # Bind mount the workspace and bashrc file
-    --volume "${REPO_ROOT_DIR}:${REPO_ROOT_DIR}"
-    --volume "${HOME}/.bashrc:${HOME}/.bashrc:ro"
-    # Set working directory and container identity
-    --workdir "${REPO_ROOT_DIR}"
-    --name "$CONTAINER_NAME"
-    --hostname "$CONTAINER_NAME"
-)
+    # Replicate a container name somewhat similar to what `docker compose run` would do if we were
+    # using `docker compose run` instead of `docker run`
+    #
+    # Last 8 chars of a hash over the "sec.ns" timestamp since epoch.
+    UNIQUE_HASH="$(date "+%s.%N" | sha256sum | cut -c1-8)"
+    CONTAINER_NAME="${IMAGE_NAME}-${IMAGE_TAG}-${UNIQUE_HASH}"
 
-docker run "${docker_args[@]}" "${IMAGE_NAME}:${IMAGE_TAG}" "$@"
+    # Run a container instance of the Docker image and tag "${IMAGE_NAME}:${IMAGE_TAG}" with the
+    # following options:
+    #
+    # * --rm = Automatically remove the container when it exits.
+    # * -it = Give the container an interactive terminal.
+    # * --env USER_ID=$(id -u) = Pass the host user's UID to the container (for gosu).
+    # * --env GROUP_ID=$(id -g) = Pass the host user's GID to the container (for gosu).
+    # * --env ... = other environmental variable options.
+    # * --volume /HOST-DIR:/CONTAINER-DIR = Bind mount the workspace directory outside the docker
+    #     container to inside the container so you can access your code.
+    # *   Also mount the host's `~/.bashrc` file to the container as read-only, to be used by the
+    #     container's ENTRYPOINT script if desired.
+    # * --workdir = Set the working directory inside the container. This is where the interactive
+    #     prompt will start.
+    # * --name = set this as the container name as shown by `docker ps` outside the container but
+    #     while it is running.
+    # * --hostname = set the container's hostname, as shown by `hostname` and in the PS1
+    #     prompt string as `user@hostname` inside the running container.
+    # * "${IMAGE_NAME}:${IMAGE_TAG}" = The name of the already-built Docker image and tag to run.
+    # * "$@" = All additional arguments passed to this script, forwarded to `docker run`.
+    #      Ex: `./docker_run.sh bash -c "cd cpp && x86_64-w64-mingw32-g++ --version"`
+    #
+    docker_args=(
+        --rm
+        -it
+        # Pass user/group IDs and names for proper file permissions (used by gosu in ENTRYPOINT)
+        --env QUIET="${QUIET}"
+        --env USER_ID="$(id -u)"
+        --env GROUP_ID="$(id -g)"
+        --env USER_NAME="$(id -un)"
+        --env GROUP_NAME="$(id -gn)"
+        # Directory to take ownership of inside the container
+        --env TAKE_OWNERSHIP_OF_DIR1="${REPO_ROOT_DIR}"
+        --env REPO_ROOT_DIR="${REPO_ROOT_DIR}"
+        # Bind mount the workspace and bashrc file
+        --volume "${REPO_ROOT_DIR}:${REPO_ROOT_DIR}"
+        --volume "${HOME}/.bashrc:${HOME}/.bashrc:ro"
+        # Set working directory and container identity
+        --workdir "${REPO_ROOT_DIR}"
+        --name "$CONTAINER_NAME"
+        --hostname "$CONTAINER_NAME"
+    )
+
+    docker run "${docker_args[@]}" "${IMAGE_NAME}:${IMAGE_TAG}" "$@"
+} # main
+
+# Determine if the script is being sourced or executed (run).
+# See:
+# 1. "eRCaGuy_hello_world/bash/if__name__==__main___check_if_sourced_or_executed_best.sh"
+# 1. My answer: https://stackoverflow.com/a/70662116/4561887
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+    # This script is being run.
+    __name__="__main__"
+else
+    # This script is being sourced.
+    __name__="__source__"
+fi
+
+# --------------------------------------------------------------------------------------------------
+# Main program entry point
+# --------------------------------------------------------------------------------------------------
+
+# Only run `main` if this script is being **run**, NOT sourced (imported).
+# - See my answer: https://stackoverflow.com/a/70662116/4561887
+if [ "$__name__" = "__main__" ]; then
+    parse_args "$@"
+    time main
+    exit $RETURN_CODE_SUCCESS
+fi
