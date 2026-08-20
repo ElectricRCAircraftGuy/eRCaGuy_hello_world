6406e7881e0d6addcfa10faff37a82babaf2753f
Gabriel Staples <ercaguy@gmail.com>
2026-08-19 19:11:41 -0700

    python/hash_a_dict.py: finalize this example: filter out the timestamp from the hash

--- python/hash_a_dict.py
+++ python/hash_a_dict.py
@@ -9,7 +9,7 @@ Aug. 2026
 
 Hash a dictionary in Python.
 
-Status: (status)
+Status: Done and works!
 
 keywords: (keywords)
 
@@ -30,7 +30,11 @@ python3 hash_a_dict.py
 
 References:
 1. https://stackoverflow.com/a/22003440/4561887
-1. [this code]
+1. [this code in my own answer]: https://stackoverflow.com/a/79996618/4561887
+1. `json.dumps()` docs: https://docs.python.org/3/library/json.html#json.dumps
+1. `json.dump()` docs to explain the args for `json.dumps()`:
+   https://docs.python.org/3/library/json.html#json.dump
+1. `hashlib.sha256()` docs: https://docs.python.org/3/library/hashlib.html#hashlib.sha256
 
 """
 
@@ -54,13 +58,33 @@ hash_hex_str = hashlib.sha256(hash_input_str.encode("utf-8")).hexdigest()
 print(f"my_dict:               {my_dict}")
 print(f"Hashable input string: {hash_input_str}")
 print(f"SHA256 hash:           {hash_hex_str}")
+print()
 
+# -------------------------------------------------------------------------------------
+# Note: to avoid hashing a certain key, such as the timestamp, filter it out like this:
+# -------------------------------------------------------------------------------------
+my_dict_filtered = {key: value for key, value in my_dict.items() if key != "timestamp"}
+hash_input_str_filtered = json.dumps(my_dict_filtered, sort_keys=True, default=str)
+hash_hex_str_filtered = hashlib.sha256(hash_input_str_filtered.encode("utf-8")).hexdigest()
+
+print(f"my_dict_filtered:      {my_dict_filtered}")
+print(f"Hashable input string: {hash_input_str_filtered}")
+print(f"SHA256 hash:           {hash_hex_str_filtered}")
+print()
 
 
 # pylint: disable-next=pointless-string-statement
 """
 SAMPLE OUTPUT:
 
+eRCaGuy_hello_world$ python/hash_a_dict.py
+my_dict:               {'name': 'John Doe', 'age': 30, 'balance': 1234.56, 'timestamp': datetime.datetime(2026, 8, 19, 12, 19, 37, 536437)}
+Hashable input string: {"age": 30, "balance": 1234.56, "name": "John Doe", "timestamp": "2026-08-19 12:19:37.536437"}
+SHA256 hash:           18c2ac9fe591a87fbd39e91ab8b7e06a83258d13fe59b85ce38caaec89a0fcd8
+
+my_dict_filtered:      {'name': 'John Doe', 'age': 30, 'balance': 1234.56}
+Hashable input string: {"age": 30, "balance": 1234.56, "name": "John Doe"}
+SHA256 hash:           381437e016e2917a6a8576521fd55ccc8627c775b1d826c4fce63a8f63addac0
 
 
 """
